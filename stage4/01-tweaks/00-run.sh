332750c2e42f12fa73c8fca4c4d5d520a87ee5fa
Charles Cross <charles@openrov.com>
2016-05-05 15:22:10 -0700

    Removed default stages 3 and 4 from master, since I only want to focus on a raspbian lite base

--- stage4/01-tweaks/00-run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-ln -sf /etc/systemd/system/autologin@.service ${ROOTFS_DIR}/etc/systemd/system/getty.target.wants/getty@tty1.service
-
-install -v -m 644 files/40-scratch.rules ${ROOTFS_DIR}/etc/udev/rules.d/
--- stage4/03-cleanup/00-run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-on_chroot sh -e - <<EOF
-apt-get clean
-EOF
