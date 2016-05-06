257d05a07069c1eafa4874dc8fafb298cfb50ff3
Charles Cross <charles@openrov.com>
2016-05-05 17:05:35 -0700

    Removed openrov bluetooth disabling stuff to make it mergeable with upstream

--- stage2/03-boot-tweaks/00-run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -ex
-
-install -m 644 files/cmdline.txt ${ROOTFS_DIR}/boot/
-install -m 644 files/config.txt ${ROOTFS_DIR}/boot/
-install -m 644 files/interfaces ${ROOTFS_DIR}/etc/network/
