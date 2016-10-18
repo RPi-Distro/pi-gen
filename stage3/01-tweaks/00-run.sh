232d0a48a041005e1ca1e44fe4ec8df1947f1c3e
Serge Schneider <serge@raspberrypi.org>
2016-10-18 15:21:37 +0100

    Remove desktop-background setting (not used by PIXEL)

--- stage3/01-tweaks/00-run.sh
+++ stage3/01-tweaks/00-run.sh
@@ -1,8 +1,3 @@
 #!/bin/bash -e
 
-on_chroot sh -e - <<EOF
-update-alternatives --install /usr/share/images/desktop-base/desktop-background \
-desktop-background /usr/share/raspberrypi-artwork/raspberry-pi-logo.png 100
-EOF
-
 rm -f ${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d/wait.conf
