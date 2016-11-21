09590eba4aa6b9a47e36288cc890f3361be0c783
Serge Schneider <serge@raspberrypi.org>
2016-11-21 18:00:51 +0000

    export-noobs: use bsdtar

--- export-noobs/prerun.sh
+++ export-noobs/prerun.sh
@@ -19,7 +19,7 @@ mkdir -p ${NOOBS_DIR}
 mount $ROOT_DEV ${STAGE_WORK_DIR}/rootfs
 mount $BOOT_DEV ${STAGE_WORK_DIR}/rootfs/boot
 
-tar -I pxz -C ${STAGE_WORK_DIR}/rootfs/boot -cpf ${NOOBS_DIR}/boot.tar.xz .
-tar -I pxz -C ${STAGE_WORK_DIR}/rootfs --one-file-system -cpf ${NOOBS_DIR}/root.tar.xz .
+bsdtar --use-compress-program pxz -C ${STAGE_WORK_DIR}/rootfs/boot -cpf ${NOOBS_DIR}/boot.tar.xz .
+bsdtar --use-compress-program pxz -C ${STAGE_WORK_DIR}/rootfs --one-file-system -cpf ${NOOBS_DIR}/root.tar.xz .
 
 unmount_image ${IMG_FILE}
