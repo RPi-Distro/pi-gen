2301d26e2bdf9bc2f6957874565f439dda153f57
Fredrik Lindqvist <landrash@gmail.com>
2017-02-25 21:12:58 +0100

    fix install of hassbian-scripts packge

--- stage3/01-tweaks/00-run.sh
+++ stage3/01-tweaks/00-run.sh
@@ -7,9 +7,10 @@ EOF
 
 install -v -o 1001 -g 1001 -d ${ROOTFS_DIR}/srv/homeassistant
 wget -O files/hassbian-scripts-0.2.deb https://github.com/home-assistant/hassbian-scripts/releases/download/v0.2/hassbian-scripts-0.2.deb
+install -v -m 600 files/hassbian-scripts-0.2.deb ${ROOTFS_DIR}/srv/homeassistant/
 
 on_chroot << EOF
-dpkg -i files/hassbian-scripts-0.2.deb
+dpkg -i /srv/homeassistant//hassbian-scripts-0.2.deb
 EOF
 
 on_chroot << \EOF
