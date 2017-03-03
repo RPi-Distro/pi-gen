daa9e35ada83e922b01afd8d029d660835fc4ea9
Serge Schneider <serge@raspberrypi.org>
2017-03-03 13:22:06 +0000

    Copy ssh and ssh.txt to boot partition

--- export-noobs/00-release/files/partition_setup.sh
+++ export-noobs/00-release/files/partition_setup.sh
@@ -16,6 +16,14 @@ sed /tmp/1/cmdline.txt -i -e "s|root=/dev/[^ ]*|root=${part2}|"
 sed /tmp/2/etc/fstab -i -e "s|^.* / |${part2}  / |"
 sed /tmp/2/etc/fstab -i -e "s|^.* /boot |${part1}  /boot |"
 
+if [ -e /mnt/ssh ]; then
+  cp /mnt/ssh /tmp/1/
+fi
+
+if [ -e /mnt/ssh.txt ]; then
+  cp /mnt/ssh.txt /tmp/1/
+fi
+
 if ! grep -q resize /proc/cmdline; then
   sed -i 's| quiet init=/usr/lib/raspi-config/init_resize.sh||' /tmp/1/cmdline.txt
 fi
