72360e5b27fa301bfd17bfe3aedd93b94e687a41
Yossi <saoron@gmail.com>
2017-04-20 09:05:13 +0300

    use modified i2samp install file

--- stage2/01-sys-tweaks/01-run.sh
+++ stage2/01-sys-tweaks/01-run.sh
@@ -142,7 +142,7 @@ echo "start_x=1" >> /boot/config.txt
 
 
 echo "========== Setup sound to I2S  ============"
-sudo curl -sS https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/i2samp.sh  | bash
+sudo curl -sS https://dride.io/code/i2samp.sh  | bash
 
 echo "========== Install mpg123  ============"
 sudo apt-get install mpg123 -y
