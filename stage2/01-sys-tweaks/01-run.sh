c0cdf6482528877091778271db3fe88ef886f0b2
Yossi <saoron@gmail.com>
2017-04-30 09:21:16 +0300

    get latests cardigan from S3 static url

--- stage2/01-sys-tweaks/01-run.sh
+++ stage2/01-sys-tweaks/01-run.sh
@@ -274,13 +274,13 @@ sudo curl -sS https://dride.io/code/i2samp.sh  | bash
 
 echo "========== Install Dride-core [Cardigan]  ============"
 cd /home
-# https://github.com/dride/Cardigan/archive/0.3.zip
+# https://s3.amazonaws.com/dride/releases/cardigan/latest.zip
 sudo mkdir Cardigan && cd Cardigan
-sudo wget -c -O "cardigan-0.3.1.zip" "https://github.com/dride/Cardigan/releases/download/0.3.1/Cardigan.zip"
-sudo unzip "cardigan-0.3.1.zip"
+sudo wget -c -O "cardigan.zip" "https://s3.amazonaws.com/dride/releases/cardigan/latest.zip"
+sudo unzip "cardigan.zip"
 
 sudo rm -R __MACOSX
-sudo rm -R cardigan-0.3.1.zip
+sudo rm -R cardigan.zip
 
 
 # make the video dir writable
