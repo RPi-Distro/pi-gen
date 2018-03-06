3f94254ca8c1145d34031d6f4b0c2ff6b08b1558
Yossi <saoron@gmail.com>
2018-03-06 16:10:42 +0200

    fix RTC was not working becouse i2c was off | close dride/dride-core#59

--- stage2/06-dride-base/00-run.sh
+++ stage2/06-dride-base/00-run.sh
@@ -210,9 +210,12 @@ sudo apt-get install python-smbus i2c-tools -y
 # TODO: turn on ISC on raspi-config...
 
 
-# add to sudo nano /boot/config.txt
+# add to /boot/config.txt
 echo "dtoverlay=i2c-rtc,ds3231,dwc2" >> /boot/config.txt
 echo "dtparam=i2c_arm=on" >> /boot/config.txt
+
+# add to /boot/config.txt
+echo "i2c-dev" >> /etc/modules
 
 
 # Remove hw-clock
