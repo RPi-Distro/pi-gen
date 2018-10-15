c217a5ea2176b600c2fe98499e1ae434751027e7
Jeffrey Paul <jp@eeqj.com>
2018-10-14 18:44:22 -0700

    lots of changes, see summary
    * default to ubuntu default keyboard layout, don't override with gb
    * run rc.local.txt from /boot if exists
    * don't export stage4/5 images or noobs
    * export stage2 -keyternal image
    * don't resize root partition on first boot
    * don't enable swap by default

--- stage2/01-sys-tweaks/01-run.sh
+++ stage2/01-sys-tweaks/01-run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-install -m 755 files/resize2fs_once	"${ROOTFS_DIR}/etc/init.d/"
-
 install -d				"${ROOTFS_DIR}/etc/systemd/system/rc-local.service.d"
 install -m 644 files/ttyoutput.conf	"${ROOTFS_DIR}/etc/systemd/system/rc-local.service.d/"
 
@@ -22,14 +20,7 @@ EOF
 if [ "${USE_QEMU}" = "1" ]; then
 	echo "enter QEMU mode"
 	install -m 644 files/90-qemu.rules "${ROOTFS_DIR}/etc/udev/rules.d/"
-	on_chroot << EOF
-systemctl disable resize2fs_once
-EOF
 	echo "leaving QEMU mode"
-else
-	on_chroot << EOF
-systemctl enable resize2fs_once
-EOF
 fi
 
 on_chroot << \EOF
