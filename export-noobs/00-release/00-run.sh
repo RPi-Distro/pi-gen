0f7e3dec0518ac8f9cde0649b55a46ebf26a289b
Serge Schneider <serge@raspberrypi.org>
2018-10-30 13:07:28 +0000

    export-noobs: fix units

--- export-noobs/00-release/00-run.sh
+++ export-noobs/00-release/00-run.sh
@@ -13,8 +13,8 @@ tar -v -c -C		files/marketing			-f "${NOOBS_DIR}/marketing.tar" .
 BOOT_SIZE="$(xz --robot -l "${NOOBS_DIR}/boot.tar.xz"  | grep totals | cut -f 5)"
 ROOT_SIZE="$(xz --robot -l "${NOOBS_DIR}/root.tar.xz"  | grep totals | cut -f 5)"
 
-BOOT_SIZE="$(( BOOT_SIZE / 1000000 + 1))"
-ROOT_SIZE="$(( ROOT_SIZE / 1000000 + 1))"
+BOOT_SIZE="$(( BOOT_SIZE / 1024 / 1024 + 1))"
+ROOT_SIZE="$(( ROOT_SIZE / 1024 / 1024 + 1))"
 
 BOOT_NOM="$(( BOOT_SIZE * 3 ))"
 ROOT_NOM="$(( ROOT_SIZE + 400 ))"
