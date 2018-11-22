03b5f1610e9e6421b10afaf51057b2f871c0a559
Simon Stürz <simon.stuerz@guh.io>
2018-11-22 13:00:10 +0100

    Add raspbian priority to apt preferences

--- stage2/00-packages/00-run-chroot.sh
+++ stage2/00-packages/00-run-chroot.sh
@@ -1,3 +1,16 @@
-echo -e "\n## nymea repo\ndeb http://repository.nymea.io stretch main\n#deb-src http://repository.nymea.io stretch main" | tee /etc/apt/sources.list.d/nymea.list
+# Add nymea repository
+echo -e "\n## nymea repo\ndeb http://repository.nymea.io stretch main raspbian\n#deb-src http://repository.nymea.io stretch main raspbian" | tee /etc/apt/sources.list.d/nymea.list
 wget -qO - http://repository.nymea.io/repository-pubkey.gpg | apt-key add -
+
+# Set repository priority (prefere packages from raspbian section
+cat <<EOM >/etc/apt/preferences.d/nymea
+Package: *
+Pin: release c=raspbian
+Pin-Priority: 700
+
+Package: *
+Pin: origin repository.nymea.io c=main
+Pin-Priority: 500
+EOM
+
 apt-get update
