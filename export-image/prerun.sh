1c9df06fc3a67aad73c3e7efe88e28972e9c92c8
Sam Tygier <samtygier@yahoo.co.uk>
2019-02-17 15:29:10 +0000

    Use different rsync options for boot and root
    When copying files to boot, options that are not supported on fat32 can cause errors. For example owner, group and xattrs.

--- export-image/prerun.sh
+++ export-image/prerun.sh
@@ -66,4 +66,5 @@ mount -v "$ROOT_DEV" "${ROOTFS_DIR}" -t ext4
 mkdir -p "${ROOTFS_DIR}/boot"
 mount -v "$BOOT_DEV" "${ROOTFS_DIR}/boot" -t vfat
 
-rsync -aHAXx --exclude var/cache/apt/archives "${EXPORT_ROOTFS_DIR}/" "${ROOTFS_DIR}/"
+rsync -aHAXx --exclude /var/cache/apt/archives --exclude /boot "${EXPORT_ROOTFS_DIR}/" "${ROOTFS_DIR}/"
+rsync -rtx "${EXPORT_ROOTFS_DIR}/boot/" "${ROOTFS_DIR}/boot/"
