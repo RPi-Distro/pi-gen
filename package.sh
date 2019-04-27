5fb5e1b68057a7025994beea11701393483171d1
Viktor Petersson <vpetersson@wireload.net>
2019-04-27 11:14:11 +0100

    Fixes packaging script.

--- package.sh
+++ package.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-cd deploy
-
 # Rename images
-for f in image_*; do
-    mv -v $f $(echo $f | sed 's/image_//g')
+for f in *.zip; do
+    mv -v ${f} $(echo ${f} | sed 's/image_//g' | sed 's/-lite//g')
 done
 
 # Add checksums
-for i in *.zip;
-    do md5sum $i > $i.md5 && sha256sum $i > $i.sha256
+for i in *.zip; do
+    md5sum ${i} > ${i}.md5
+    sha256sum ${i} > ${i}.sha256
 done
