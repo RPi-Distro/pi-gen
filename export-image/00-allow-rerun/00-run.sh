0cdec860269dc3e10aad086a27191a1911d1579e
Serge Schneider <serge@raspberrypi.org>
2019-06-07 17:30:05 +0100

    Disable ld.so.preload while building

--- export-image/00-allow-rerun/00-run.sh
+++ export-image/00-allow-rerun/00-run.sh
@@ -3,3 +3,7 @@
 if [ ! -x "${ROOTFS_DIR}/usr/bin/qemu-arm-static" ]; then
 	cp /usr/bin/qemu-arm-static "${ROOTFS_DIR}/usr/bin/"
 fi
+
+if [ -e "${ROOTFS_DIR}/etc/ld.so.preload" ]; then
+	mv "${ROOTFS_DIR}/etc/ld.so.preload" "${ROOTFS_DIR}/etc/ld.so.preload.disabled"
+fi
--- export-image/04-finalise/01-run.sh
+++ export-image/04-finalise/01-run.sh
@@ -15,6 +15,12 @@ fi
 rm -f "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache"
 rm -f "${ROOTFS_DIR}/usr/bin/qemu-arm-static"
 
+if [ "${USE_QEMU}" != "1" ]; then
+	if [ -e "${ROOTFS_DIR}/etc/ld.so.preload.disabled" ]; then
+		mv "${ROOTFS_DIR}/etc/ld.so.preload.disabled" "${ROOTFS_DIR}/etc/ld.so.preload"
+	fi
+fi
+
 rm -f "${ROOTFS_DIR}/etc/network/interfaces.dpkg-old"
 
 rm -f "${ROOTFS_DIR}/etc/apt/sources.list~"
