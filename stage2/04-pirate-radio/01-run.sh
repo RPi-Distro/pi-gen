93297f99f23a8bf87630203aaecf6fdc7840e700
piradio <b@kuma.la>
2019-06-15 15:52:52 +0200

    Add MPD with its corresponding services.

--- stage2/04-pirate-radio/01-run.sh
+++ stage2/04-pirate-radio/01-run.sh
@@ -1,10 +1,30 @@
 #!/bin/bash -e
 
+# set hostname
+install -v -m 644 files/etc/hostname					"${ROOTFS_DIR}/etc/"
+install -v -m 644 files/etc/hosts					"${ROOTFS_DIR}/etc/"
+
 # pulseaudio
 install -v -m 644 files/etc/systemd/system/pulseaudio.service		"${ROOTFS_DIR}/etc/systemd/system/"
 install -v -m 644 files/etc/pulse/client.conf				"${ROOTFS_DIR}/etc/pulse/"
 install -v -m 644 files/etc/pulse/default.pa				"${ROOTFS_DIR}/etc/pulse/"
 
 # pivumeter
-cp -rf files/pivumeter							"${ROOTFS_DIR}/root/"
-install -v -m 644 files/pivumeter/dependencies/etc/asound.conf		"${ROOTFS_DIR}/etc/"
+git clone https://github.com/pimoroni/pivumeter.git			"${ROOTFS_DIR}/root/pivumeter"
+install -v -m 644 "${ROOTFS_DIR}/root/pivumeter/dependencies/etc/asound.conf"		"${ROOTFS_DIR}/etc/"
+
+# mpd
+git clone https://github.com/Mic92/python-mpd2.git			"${ROOTFS_DIR}/root/python-mpd2"
+install -v -m 644 files/etc/mpd.conf					"${ROOTFS_DIR}/etc/"
+if [ -f ../../my-playlist.m3u ]; then
+	install -v -m 644 ../../my-playlist.m3u				"${ROOTFS_DIR}/var/lib/mpd/playlists/my-playlist.m3u"
+fi
+
+# physical interface
+install -v -m 644 files/etc/systemd/system/radio-interface.service	"${ROOTFS_DIR}/etc/systemd/system/"
+mkdir "${ROOTFS_DIR}/usr/local/lib/radio-interface/"
+install -v -m 644 files/interface.py					"${ROOTFS_DIR}/usr/local/lib/radio-interface/"
+
+# web interface
+install -v -m 644 files/etc/systemd/system/ympd.service			"${ROOTFS_DIR}/etc/systemd/system/"
+git clone https://github.com/notandy/ympd.git				"${ROOTFS_DIR}/root/ympd"
--- stage2/04-pirate-radio/02-run-chroot.sh
+++ stage2/04-pirate-radio/02-run-chroot.sh
@@ -1,9 +1,31 @@
 #!/bin/bash -e
 
+# pulseaudio
 systemctl enable pulseaudio
 
+#pivumeter
 cd /root/pivumeter
 aclocal && libtoolize
 autoconf && automake --add-missing
 ./configure && make
 make install
+
+# mpd
+cd /root/python-mpd2
+python3 setup.py install
+
+# physical interface
+useradd -r -s /bin/false radio-interface
+adduser radio-interface gpio
+echo 'radio-interface ALL=(ALL) NOPASSWD: /sbin/shutdown' > /etc/sudoers.d/010_radio-interface-shutdown
+chmod 0440 /etc/sudoers.d/010_radio-interface-shutdown
+systemctl enable radio-interface
+
+# web interface
+cd /root/ympd
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX:PATH=/usr
+make
+make install
+systemctl enable ympd
