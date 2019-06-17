150e25c4f8123a4c9c63e8dca1b4737fa6c1135c
Serge Schneider <serge@raspberrypi.org>
2019-06-17 10:32:11 +0100

    Revert "Java 8 workaround"
    This reverts commit 3682a747bdf319a34fcce7cad9771a1a3ead34ec.

--- stage5/00-install-extras/00-run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-on_chroot << EOF
-apt-get install openjdk-11-jre-headless -y --no-install-recommends
-#update-binfmts --package openjdk-11 --remove jar /usr/bin/jexec
-apt-get purge openjdk-11-jre-headless openjdk-8-jre-headless+ -y
-apt-mark auto openjdk-8-jre-headless
-EOF
