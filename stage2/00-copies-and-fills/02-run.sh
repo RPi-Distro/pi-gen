f891d1084fba2f4cc319677c289c898de534e636
Xerxes Ranby <xerxes@gudinna.com>
2019-07-01 16:46:34 +0000

    stage2/00-copies-and-fills/02-run.sh: Fix re-runs of script
    Check that file exist before move.

--- stage2/00-copies-and-fills/02-run.sh
+++ stage2/00-copies-and-fills/02-run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -e
 
-mv "${ROOTFS_DIR}/etc/ld.so.preload" "${ROOTFS_DIR}/etc/ld.so.preload.disabled"
+if [ -f "${ROOTFS_DIR}/etc/ld.so.preload" ]; then
+   mv "${ROOTFS_DIR}/etc/ld.so.preload" "${ROOTFS_DIR}/etc/ld.so.preload.disabled"
+fi
+
