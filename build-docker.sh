a9e0006a8af67b8445e7f19dad8f42ba9cb228ea
Giovanni Garufi <giovanni@balena.io>
2019-08-20 12:49:19 +0200

    Prepare host kernel before running docker build
    Enable binfmt_misc before the docker build
    Also create loop block devices before docker build as losetup -f will
    otherwise fail.
    
    Change-type: patch
    Signed-off-by: Giovanni Garufi <giovanni@balena.io>

--- build-docker.sh
+++ build-docker.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -eu
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc && echo 1 > /proc/sys/fs/binfmt_misc/status
+for i in $(seq 1 10); do
+  mknod -m640 /dev/loop$i b 7 $i
+done
+
 BUILD_OPTS="$*"
 
 DOCKER="docker"
