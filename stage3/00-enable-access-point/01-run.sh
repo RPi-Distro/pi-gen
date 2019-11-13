2c5534d6eaa540683bc9e34e3526d42de226cb4e
German Ferrero <ferrero.gf@gmail.com>
2019-11-13 13:32:27 -0300

    fix paths

--- stage3/00-enable-access-point/01-run.sh
+++ stage3/00-enable-access-point/01-run.sh
@@ -3,8 +3,8 @@
 # Setup an static ip to wlan0
 cat files/dhcpcd.conf >> ${ROOTFS_DIR}/etc/dhcpcd.conf
 # Setup dhcp server for rpi to serve clients
-cat files/dnsmasq.conf >> /etc/dnsmasq.conf
+cat files/dnsmasq.conf >> ${ROOTFS_DIR}/etc/dnsmasq.conf
 # Setup access point in hostapd/hostapd.conf
-cat files/hostapd.conf >> /etc/hostapd/hostapd.conf
+cat files/hostapd.conf >> ${ROOTFS_DIR}/etc/hostapd/hostapd.conf
 # Point default/hostapd conf to hostapd/hostapd.conf
 sed -i "s/#DAEMON_CONF=\"\"/DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"/" "{$ROOTFS_DIR}/etc/default/hostapd"
