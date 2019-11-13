e1a3a0ae386dc9358cda1688689a7b403c509cdd
German Ferrero <ferrero.gf@gmail.com>
2019-11-13 15:10:40 -0300

    fix another syntax error

--- stage3/00-enable-access-point/01-run.sh
+++ stage3/00-enable-access-point/01-run.sh
@@ -7,4 +7,4 @@ cat files/dnsmasq.conf >> ${ROOTFS_DIR}/etc/dnsmasq.conf
 # Setup access point in hostapd/hostapd.conf
 cat files/hostapd.conf >> ${ROOTFS_DIR}/etc/hostapd/hostapd.conf
 # Point default/hostapd conf to hostapd/hostapd.conf
-sed -i "s@#DAEMON_CONF=\"\"@DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"@" "{$ROOTFS_DIR}/etc/default/hostapd"
+sed -i "s@#DAEMON_CONF=\"\"@DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"@" "${ROOTFS_DIR}/etc/default/hostapd"
