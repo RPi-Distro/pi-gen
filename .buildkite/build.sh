cc0d01cf6c57c1649b36568b4bc45b152981d4a2
David Canas <david@learningequality.org>
2019-12-10 19:07:23 -0800

    Adding ability to preserve existing container

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -1,8 +1,21 @@
 #! /usr/bin/env bash
 
+# Could move this type of setup to hooks
 if [[ $LE_TRIGGERED_FROM_BUILD_ID ]]
 then
+  echo "--- Downloading debian package from Buildkite"
   buildkite-agent artifact download --build $LE_TRIGGERED_FROM_BUILD_ID 'dist/*.deb' .
+  export LOCAL_KOLIBRI_PACKAGE=$PWD/dist/*.deb
+else
+  echo "Image will use Debian Package from Launchpad"
 fi
 
-./build-docker.sh
+
+if [[ $(docker ps -a | grep pi-gen) ]]
+then
+  echo "--- Building Pi Image with cache :money:"
+  PRESERVE_CONTAINER=1 ./build-docker.sh
+else
+  echo "--- Building Pi Image"
+  ./build-docker.sh
+fi
