2f0c10e9e0200ddef23bf66b3a77d8a263432e83
David Canas <david@learningequality.org>
2019-12-10 20:00:18 -0800

    Installing custom debian package. Sourced from Buildkite when appropriate.

--- stage2/03-kolibri-server/04-run.sh
+++ stage2/03-kolibri-server/04-run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -e
+
+if -a /dist/*.deb
+then
+  dpkg -i /dist/*.deb
+fi
+
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.kolibri"
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/KOLIBRI_DATA/content"
 install -m 644 -o 1000 -g 1000  options.ini "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.kolibri/"
