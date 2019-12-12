9249f2defcdc5ffbbaa06095f6ff04c6906ef607
David Canas <david@learningequality.org>
2019-12-11 16:04:48 -0800

    Add upload step to build script.

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -14,3 +14,15 @@ echo "--- Building Pi Image"
 PRESERVE_CONTAINER=1 
 CONTINUE=1 
 . ./build-docker.sh
+
+echo "--- Uploading artifacts"
+
+# Moving to another folder to match convention of other installers
+mv deploy dist
+
+if [[ $LE_TRIGGERED_FROM_JOB_ID ]]
+then
+  UPLOAD_BUILD_FLAGS="--job $LE_TRIGGERED_FROM_JOB_ID"
+fi
+
+buildkite-agent artifact upload dist/*.zip $UPLOAD_BUILD_FLAGS
