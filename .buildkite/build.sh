358c1eb577c9e0862d95415eb528ccc14a9e07c0
David Canas <david@learningequality.org>
2019-12-13 20:17:38 -0800

    Removing docker volume mounting Build file copies in entire directory by default

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -5,7 +5,6 @@ if [[ $LE_TRIGGERED_FROM_BUILD_ID ]]
 then
   echo "--- Downloading debian package from Buildkite"
   buildkite-agent artifact download --build $LE_TRIGGERED_FROM_BUILD_ID 'dist/*.deb' .
-  DOCKER_RUN_OPTS="-v $PWD/dist/:/dist:ro"
 else
   echo "Image will use Debian Package from Launchpad"
 fi
--- stage2/03-kolibri-server/04-run.sh
+++ stage2/03-kolibri-server/04-run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -e
 
-if [[ $(find /dist -maxdepth 1 -name '*.deb' -print -quit) ]]
+set -x
+
+if [[ $(find /pi-gen/dist -maxdepth 1 -name '*.deb' -print -quit) ]]
 then
   echo "--- Using Buildkite-provided Kolibri deb"
-  cp -r /dist/*.deb $ROOTFS_DIR
-  on_chroot < dpkg -i /dist/*.deb
+  
+  # All files copied at build stage to /pi-gen
+  mv /pi-gen/dist/*.deb $ROOTFS_DIR
+  on_chroot < dpkg -i *.deb
 
   echo "--- Continuing Image Build "
 fi
