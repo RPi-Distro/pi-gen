2134c188148985b5a6170293f2dd236c5398b63f
José L. Redrejo Rodríguez <jredrejo@debian.org>
2019-12-16 14:20:29 +0100

    fix chroot with dpkg

--- stage2/03-kolibri-server/04-run.sh
+++ stage2/03-kolibri-server/04-run.sh
@@ -5,10 +5,13 @@ set -x
 if [[ $(find /pi-gen/dist -maxdepth 1 -name '*.deb' -print -quit) ]]
 then
   echo "--- Using Buildkite-provided Kolibri deb"
-  
+
   # All files copied at build stage to /pi-gen
   mv /pi-gen/dist/*.deb $ROOTFS_DIR
-  on_chroot < dpkg -i /*.deb
+
+  on_chroot << EOF
+  dpkg -i /*.deb
+EOF
 
   echo "--- Continuing Image Build "
 fi
