02d72b390ef5a6be6e9eff5bfc24aa284435a0d3
David Canas <david@learningequality.org>
2019-12-29 10:16:35 -0800

    Uploading all images that match, rather than more complex scripting.
    This reverts commit 5c6afca379a13eb81b268c61b52d852e73ec0676.

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -16,18 +16,15 @@ CONTINUE=1 PRESERVE_CONTAINER=1 ./build-docker.sh
 
 echo "--- Uploading artifacts"
 
-# Moving to another folder to match convention of other installers
 rm -rf dist/*
 
-# Creates a bash array. Using ls because it sorts by name by default, and images are prefixed with date
-IMAGE=($(ls ./deploy/*.zip))
-
-mv deploy/$IMAGE dist/
+# Moving to another folder to match convention of other installers
+mv deploy/* dist/
 
 if [[ $LE_TRIGGERED_FROM_JOB_ID ]]
 then
-  buildkite-agent artifact upload dist/*.zip --job $LE_TRIGGERED_FROM_JOB_ID
+  buildkite-agent artifact upload "dist/*.zip" --job $LE_TRIGGERED_FROM_JOB_ID
 fi
 
 # Upload twice - once to pipeline, and once to this one
-buildkite-agent artifact upload dist/*.zip
+buildkite-agent artifact upload "dist/*.zip"
