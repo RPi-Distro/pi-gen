c507c2109c6de0443b6c767961a1ff8729efdd01
Samuel Cozannet <samuel.cozannet@madeden.com>
2020-01-27 22:12:56 +0000

    fix install of node and first boot

--- stageX/01-nodejs/01-run.sh
+++ stageX/01-nodejs/01-run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
 on_chroot << EOF
-ln -sf "$(which nodejs)" /usr/bin/node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
 EOF
--- stageX/04-first-boot/01-run.sh
+++ stageX/04-first-boot/01-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 install -m 755 files/firstboot.sh "${ROOTFS_DIR}/boot/firstboot.sh"
-install -m 755 files/firstboot.service "${ROOTFS_DIR}/etc/systemd/system/firstboot.sh"
+install -m 755 files/firstboot.service "${ROOTFS_DIR}/etc/systemd/system/firstboot.service"
 
 on_chroot << EOF
 systemctl enable firstboot.service
