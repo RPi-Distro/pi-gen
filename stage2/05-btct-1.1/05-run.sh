618280f9442895fdbebb84bdc7e80c3f1e5f8e6b
go140point6 <ryan@go140point6.com>
2020-02-03 09:43:27 -0800

    added lite version, staged bootstrap

---
+++ stage2/05-btct-1.1/05-run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -e
+
+install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/btct/"
+install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/btct/bin/"
+
+install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.btct/"
+install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.btct/blocks/"
+install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.btct/blocks/index/"
+install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.btct/chainstate/"
+install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.btct/sporks/"
+
+install -v -o 1000 -g 1000 -m 755 files/btct* "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/btct/bin/"
+
+install -v -o 1000 -g 1000 -m 700 bootstrap/blocks/* "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.btct/blocks/"
+install -v -o 1000 -g 1000 -m 700 bootstrap/index/* "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.btct/blocks/index/"
+install -v -o 1000 -g 1000 -m 700 bootstrap/chainstate/* "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.btct/chainstate/"
+install -v -o 1000 -g 1000 -m 700 bootstrap/sporks/* "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.btct/sporks/" 
