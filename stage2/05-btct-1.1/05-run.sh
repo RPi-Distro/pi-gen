55e1417bb6fc84b8b3e012a66f7d9c2f04010c35
go140point6 <ryan@go140point6.com>
2020-02-04 09:40:33 -0800

    add pi-zero binaries

--- stage2/05-btct-1.1/05-run.sh
+++ stage2/05-btct-1.1/05-run.sh
@@ -2,6 +2,7 @@
 
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/btct/"
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/btct/bin/"
+install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/btct/bin/pi-zero"
 
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.btct/"
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.btct/blocks/"
@@ -10,6 +11,7 @@ install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.btct/chain
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.btct/sporks/"
 
 install -v -o 1000 -g 1000 -m 755 files/btct* "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/btct/bin/"
+install -v -o 1000 -g 1000 -m 755 files/pi-zero/btct* "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/btct/bin/pi-zero/"
 
 install -v -o 1000 -g 1000 -m 700 bootstrap/blocks/* "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.btct/blocks/"
 install -v -o 1000 -g 1000 -m 700 bootstrap/index/* "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.btct/blocks/index/"
