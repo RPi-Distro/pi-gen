dc289e4a723b5598ef3f250064108c9d22ce488a
Serge Schneider <serge@raspberrypi.org>
2020-02-20 18:43:05 +0000

    aarch64

--- stage0/prerun.sh
+++ stage0/prerun.sh
@@ -1,10 +1,5 @@
 #!/bin/bash -e
 
-if [ "$RELEASE" != "bookworm" ]; then
-	echo "WARNING: RELEASE does not match the intended option for this branch."
-	echo "         Please check the relevant README.md section."
-fi
-
-if [ ! -d "${ROOTFS_DIR}" ] || [ "${USE_QCOW2}" = "1" ]; then
+if [ ! -d "${ROOTFS_DIR}" ]; then
 	bootstrap ${RELEASE} "${ROOTFS_DIR}" http://deb.debian.org/debian/
 fi
