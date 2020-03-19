7194187c216d6a3b05f0ddfef9242e758345f5e0
Ani Balasubramaniam <ani@anionline.me>
2020-03-18 18:20:48 -0700

    1. Update config to remove a stage list, opt to use SKIP(_IMAGES) instead to build the lite-image. 2. Removed auto-login script which isn't needed.

--- stage2/04-custom-tweaks/01-run.sh
+++ stage2/04-custom-tweaks/01-run.sh
@@ -8,9 +8,3 @@ on_chroot << EOF
 curl -sSL get.docker.com | sh
 usermod -aG docker ${FIRST_USER_NAME}
 EOF
-
-# Ensure ${FIRST_USER_NAME} is set up for auto-login
-echo "Setting up ${FIRST_USER_NAME} for auto-login"
-on_chroot << EOF
-	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_behaviour B4
-EOF
