6333ed92a57b94a0f8fbeed7f24eb1b03dcb4399
Romain Bazile <gromain.baz@gmail.com>
2020-05-30 18:56:19 +0200

    Solve the space in base path problem
    If there is a space in the base anywhere, the scripts fails to start. 
    This is due to the default separator used in bash (which is a space). 
    This default separator can be change by setting the variable $IFS.
    In `build.sh`, the old IFS variable is saved and then replaced by 
    `\n\b`. At the end of the script, the saved IFS is set back.
    
    In `build-docker.sh`, a simple double quote was enough.

--- build.sh
+++ build.sh
@@ -229,7 +229,10 @@ fi
 mkdir -p "${WORK_DIR}"
 log "Begin ${BASE_DIR}"
 
-STAGE_LIST=${STAGE_LIST:-${BASE_DIR}/stage*}
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
+STAGE_LIST=${STAGE_LIST:-"${BASE_DIR}"/stage*}
 
 for STAGE_DIR in $STAGE_LIST; do
 	STAGE_DIR=$(realpath "${STAGE_DIR}")
@@ -261,3 +264,5 @@ if [ -x ${BASE_DIR}/postrun.sh ]; then
 fi
 
 log "End ${BASE_DIR}"
+
+IFS=$SAVEIFS
