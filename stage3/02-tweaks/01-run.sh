0f8007b3cd9ca4d21b3bc12b40ea497e579f2e5a
Fayaaz <fayaaz.ahmed@gmail.com>
2020-07-27 02:16:53 +0100

    Realtime scheduling fix in security.conf

--- stage3/02-tweaks/01-run.sh
+++ stage3/02-tweaks/01-run.sh
@@ -1,7 +1,7 @@
 # Enable ssh.
 touch ${ROOTFS_DIR}/boot/ssh
 
-# Boot to gui by default
+# Boot to graphical by default
 on_chroot << EOF
 	systemctl set-default graphical.target
 	ln -fs /lib/systemd/system/getty@.service /etc/systemd/system/getty.target.wants/getty@tty1.service
@@ -9,6 +9,10 @@ EOF
 
 install -m 644 files/autologin.conf ${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d/autologin.conf
 
+
 # Set up sudoers.d for user patch
 rm -f ${ROOTFS_DIR}/etc/sudoers.d/010_pi-nopasswd
 install -m 440 files/010_pi-nopasswd ${ROOTFS_DIR}/etc/sudoers.d/
+
+echo @audio - memlock 256000 >> ${ROOTFS_DIR}/etc/security/limits.conf
+echo @audio - rtprio 75 >> ${ROOTFS_DIR}/etc/security/limits.conf
