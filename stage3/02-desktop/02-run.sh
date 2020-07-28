10380727e9ddf973c88c08c25c0d388ad5482766
Fayaaz <fayaaz.ahmed@gmail.com>
2020-07-28 03:41:59 +0100

    Fix permissions some more...

--- stage3/02-desktop/02-run.sh
+++ stage3/02-desktop/02-run.sh
@@ -1,6 +1,7 @@
 # Add i3 config file
-mkdir -p -m 644 ${ROOTFS_DIR}/home/pi/.config/i3/
+mkdir -p -m 755 ${ROOTFS_DIR}/home/pi/.config/i3/
 install -m 755 files/i3.conf ${ROOTFS_DIR}/home/pi/.config/i3/config
 on_chroot << EOF
     chown -R pi:root /home/pi/.config/i3/
+    chmod -R 755 /home/pi/.config/
 EOF
