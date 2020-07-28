d69999829814b65a6bfad10569a8bbd75b6e97b4
Fayaaz <fayaaz.ahmed@gmail.com>
2020-07-28 16:30:47 +0100

    Fix permissions for .config dir

--- stage3/02-desktop/02-run.sh
+++ stage3/02-desktop/02-run.sh
@@ -2,6 +2,6 @@
 mkdir -p -m 755 ${ROOTFS_DIR}/home/pi/.config/i3/
 install -m 755 files/i3.conf ${ROOTFS_DIR}/home/pi/.config/i3/config
 on_chroot << EOF
-    chown -R pi:root /home/pi/.config/i3/
+    chown -R pi:root /home/pi/.config/
     chmod -R 755 /home/pi/.config/
 EOF
