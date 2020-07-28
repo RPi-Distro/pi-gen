07ddfa4c0b2152205e0d82bcef4bcdd15bb0cde9
Fayaaz <fayaaz.ahmed@gmail.com>
2020-07-28 13:37:36 +0100

    Comment out mixxx cfg

--- stage3/02-desktop/03-run.sh
+++ stage3/02-desktop/03-run.sh
@@ -1,10 +1,11 @@
 # Do mixxx stuff like copy the udev rules and config file
-mkdir -p -m 755 ${ROOTFS_DIR}/home/pi/.mixxx
-install -m 755 files/mixxx.cfg ${ROOTFS_DIR}/home/pi/.mixxx/mixxx.cfg
-on_chroot << EOF
-    chown -R pi:root /home/pi/.mixxx
-    chmod -R 755 /home/pi/.mixxx/
-EOF
+# mkdir -p -m 755 ${ROOTFS_DIR}/home/pi/.mixxx
+# install -m 755 files/mixxx.cfg ${ROOTFS_DIR}/home/pi/.mixxx/mixxx.cfg
+# on_chroot << EOF
+#     chown -R pi:root /home/pi/.mixxx
+#     chmod -R 755 /home/pi/.mixxx/
+# EOF
+# Commented out because Mixxx complains about lack of db if you do this
 
 install -m 644 files/udev.mixxx ${ROOTFS_DIR}/etc/udev/rules.d/69-mixxx-usb-uaccess.rules
 
