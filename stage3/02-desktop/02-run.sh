864979095be19ab1fb4f25752dccc779882b4cee
Fayaaz <fayaaz.ahmed@gmail.com>
2020-07-28 01:53:59 +0100

    Add usbmount and mixxx config and udev rules

--- stage3/02-desktop/02-run.sh
+++ stage3/02-desktop/02-run.sh
@@ -1,2 +1,6 @@
 # Add i3 config file
-install -m 440 /files/i3.conf ${ROOTFS_DIR}/home/pi/.config/i3
+install -m 755 files/i3.conf ${ROOTFS_DIR}/home/pi/.config/i3
+on_chroot << EOF
+    chown -R pi:root /home/pi/.config/
+EOF
+
