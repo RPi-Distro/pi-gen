0a8d3e83545cc974a69c7a0e76f32118d4212c1a
Fayaaz <fayaaz.ahmed@gmail.com>
2020-07-28 01:53:59 +0100

    Add usbmount and mixxx config and udev rules

---
+++ stage3/02-desktop/03-run.sh
@@ -0,0 +1,7 @@
+# Do mixxx stuff like copy the udev rules and config file
+install -m 644 files/mixxx.cfg ${ROOTFS_DIR}/home/pi/.mixxx/mixxx.cfg
+install -m 440 files/udev.mixxx ${ROOTFS_DIR}/etc/udev/rules.d/69-mixxx-usb-uaccess.rules
+
+# USB Mount
+mkdir -m 644 ${ROOTFS_DIR}/etc/systemd/system/systemd-udevd.service.d
+install -m 644 files/00-usbmountflags.conf ${ROOTFS_DIR}/etc/systemd/system/systemd-udevd.service.d/00-usbmountflags.conf
