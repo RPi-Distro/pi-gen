4203bc15197d7fa664233e7fcbe2f01f1ef72e73
Raphael Nestler <raphael.nestler@sensirion.com>
2020-08-06 17:23:35 +0200

    Use '-n' instead of '-v' to test if WPA_COUNTRY is set
    Apparently -v doesn't work in all shells.

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -27,7 +27,7 @@ fi
 
 # Disable wifi on 5GHz models if WPA_COUNTRY is not set
 mkdir -p "${ROOTFS_DIR}/var/lib/systemd/rfkill/"
-if [ -v WPA_COUNTRY ]; then
+if [ -n "$WPA_COUNTRY" ]; then
     echo 0 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-3f300000.mmcnr:wlan"
     echo 0 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-fe300000.mmcnr:wlan"
 else
