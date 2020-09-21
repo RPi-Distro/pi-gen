365ebf544836074622202b675bde408f2de88854
Jerry Vonau <jvonau3@gmail.com>
2020-09-21 03:37:02 +0100

    intergrate rng-tools update

--- stage2/01-sys-tweaks/01-run.sh
+++ stage2/01-sys-tweaks/01-run.sh
@@ -2,6 +2,8 @@
 
 install -m 755 files/resize2fs_once	"${ROOTFS_DIR}/etc/init.d/"
 
+install -m 644 files/rng-tools.service	"${ROOTFS_DIR}/etc/systemd/system/"
+
 install -d				"${ROOTFS_DIR}/etc/systemd/system/rc-local.service.d"
 install -m 644 files/ttyoutput.conf	"${ROOTFS_DIR}/etc/systemd/system/rc-local.service.d/"
 
@@ -15,6 +17,7 @@ on_chroot << EOF
 systemctl disable hwclock.sh
 systemctl disable nfs-common
 systemctl disable rpcbind
+systemctl enable rng-tools
 if [ "${ENABLE_SSH}" == "1" ]; then
 	systemctl enable ssh
 else
