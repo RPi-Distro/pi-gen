ee3d25a787bf9a39d0a00f36de30e46ba3a25836
Carl McAlwane <carl@rpi4b-2.1stcall.uk>
2020-09-24 20:39:53 +0100

    expire password

--- stage1/01-sys-tweaks/00-run.sh
+++ stage1/01-sys-tweaks/00-run.sh
@@ -10,6 +10,7 @@ if ! id -u ${FIRST_USER_NAME} >/dev/null 2>&1; then
 fi
 echo "${FIRST_USER_NAME}:${FIRST_USER_PASS}" | chpasswd
 echo "root:root" | chpasswd
+passwd --expire ${FIRST_USER_NAME}
 EOF
 
-
+install -m 644 files/.bash_aliases "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.bash_aliases"
