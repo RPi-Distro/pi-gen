4e5094eb845d2a65e594734a2d5615f97d2a9407
Carl McAlwane <carl@1stcall.uk>
2020-09-26 22:52:32 +0100

    Install with owner/group

--- stage2/01-sys-tweaks/01-run.sh
+++ stage2/01-sys-tweaks/01-run.sh
@@ -11,8 +11,8 @@ install -m 644 files/console-setup   	"${ROOTFS_DIR}/etc/default/"
 
 install -m 755 files/rc.local		"${ROOTFS_DIR}/etc/"
 
-install -m 644 files/.gitconfig		"${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
-chown 1000:1000 "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.gitconfig"
+install -m 644 -o 1000 -g 1000 files/.gitconfig		"${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
+#chown 1000:1000 "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.gitconfig"
 
 if [ -n "${PUBKEY_SSH_FIRST_USER}" ]; then
 	install -v -m 0700 -o 1000 -g 1000 -d "${ROOTFS_DIR}"/home/"${FIRST_USER_NAME}"/.ssh
