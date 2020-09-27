59cddaff780ed445898a47bb16f86a2c8f3ed106
Carl McAlwane <carl@1stcall.uk>
2020-09-27 09:50:42 +0100

    Made ARCH configurable in config

--- build.sh
+++ build.sh
@@ -207,6 +207,7 @@ export QUILT_NO_DIFF_TIMESTAMPS=1
 export QUILT_REFRESH_ARGS="-p ab"
 
 export REPOSITORY_URL="${REPOSITORY_URL:-http://deb.debian.org/debian/}"
+export ARCH="${ARCH}-armhf"
 
 # shellcheck source=scripts/common
 source "${SCRIPT_DIR}/common"
