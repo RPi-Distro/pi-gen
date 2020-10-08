3ac6e8a6ce9ebc63f340bdad471199f242871ee4
Carl McAlwane <carl@1stcall.uk>
2020-10-08 13:05:48 +0100

    reinstated adding hostname to hosts file.

--- stage1/02-net-tweaks/00-run.sh
+++ stage1/02-net-tweaks/00-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 echo "${TARGET_HOSTNAME}" > "${ROOTFS_DIR}/etc/hostname"
-#echo "127.0.1.1		${TARGET_HOSTNAME}" >> "${ROOTFS_DIR}/etc/hosts"
+echo "127.0.1.1		${TARGET_HOSTNAME}" >> "${ROOTFS_DIR}/etc/hosts"
 printf "%s\t%s\t%s\n" '192.168.0.110' 'rpi4b-1.1stcall.uk' 'rpi4b-1' | tee -a "${ROOTFS_DIR}/etc/hosts"
 printf "%s\t%s\t%s\n" '192.168.0.120' 'rpi4b-2.1stcall.uk' 'rpi4b-2' | tee -a "${ROOTFS_DIR}/etc/hosts"
 printf "%s\t%s\t%s\n" '192.168.0.130' 'rpi4b-3.1stcall.uk' 'rpi4b-3' | tee -a "${ROOTFS_DIR}/etc/hosts"
