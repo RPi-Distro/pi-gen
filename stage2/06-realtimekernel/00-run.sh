8fb8bcccbfc017b24d690e10b8190358022292c7
Bruno Gola <me@bgo.la>
2020-11-03 16:03:00 +0100

    dont use sudo

--- stage2/06-realtimekernel/00-run.sh
+++ stage2/06-realtimekernel/00-run.sh
@@ -2,14 +2,14 @@ cd /tmp
 mkdir rtkernel && cd rtkernel
 wget https://users.notam.no/~thomj/rt-kernel.tar.gz && tar xzf rt-kernel.tar.gz
 cd boot
-sudo cp -rd * ${ROOTFS_DIR}/boot/
+cp -rd * "${ROOTFS_DIR}/boot/"
 cd ../lib
-sudo cp -dr * ${ROOTFS_DIR}/lib/
+cp -dr * "${ROOTFS_DIR}/lib/"
 cd ../overlays
-sudo cp -d * ${ROOTFS_DIR}/boot/overlays
+cp -d * "${ROOTFS_DIR}/boot/overlays"
 cd ..
-sudo cp -d bcm* ${ROOTFS_DIR}/boot/
+cp -d bcm* "${ROOTFS_DIR}/boot/"
 
 echo '
 [all]
-kernel=kernel7l' >> ${ROOTFS_DIR}/boot/config.txt
+kernel=kernel7l' >> "${ROOTFS_DIR}/boot/config.txt"
