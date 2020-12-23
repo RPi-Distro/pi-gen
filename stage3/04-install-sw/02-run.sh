c711b501ffc8cc2e0384bbf58e3b1cb30e26a607
kdoren <kevin@doren.org>
2020-12-23 00:08:37 -0800

    clean up comments

--- stage3/04-install-sw/02-run.sh
+++ stage3/04-install-sw/02-run.sh
@@ -2,9 +2,7 @@
 
 # install noVNC
 git clone --depth 1 --branch v1.2.0 https://github.com/novnc/noVNC.git ${ROOTFS_DIR}/usr/local/noVNC/
-#git clone https://github.com/novnc/noVNC.git ${ROOTFS_DIR}/usr/local/noVNC/
 git clone --depth 1 --branch v0.9.0 https://github.com/novnc/websockify.git ${ROOTFS_DIR}/usr/local/noVNC/utils/websockify
-#git clone https://github.com/novnc/websockify ${ROOTFS_DIR}/usr/local/noVNC/utils/websockify
 cp ${ROOTFS_DIR}/usr/local/noVNC/vnc.html ${ROOTFS_DIR}/usr/local/noVNC/index.html
 
 # install novnc service file
