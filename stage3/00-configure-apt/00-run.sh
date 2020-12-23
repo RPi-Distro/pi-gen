fd0b50ba1e1fa16c31bf3618b7ab2295ddc9c9f2
kdoren <kevin@doren.org>
2020-12-22 17:44:14 -0800

    add jambox-project apt repo

--- stage3/00-configure-apt/00-run.sh
+++ stage3/00-configure-apt/00-run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
+install -m 644 files/jambox-project.list ${ROOTFS_DIR}/etc/apt/sources.list.d/
 install -m 644 files/blokas.list ${ROOTFS_DIR}/etc/apt/sources.list.d/
 
+on_chroot apt-key add - < files/repo.jambox-project.com.gpg
 on_chroot apt-key add - < files/blokas.gpg.key
 on_chroot << EOF
 apt-get update
