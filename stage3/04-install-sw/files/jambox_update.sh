459bb2b7044f6c5e7a1b4e2d6cb9f59450d5a13f
kdoren <kevin@doren.org>
2021-01-03 00:45:56 -0800

    force -y and add jamtrip to update script

--- stage3/04-install-sw/files/jambox_update.sh
+++ stage3/04-install-sw/files/jambox_update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo 'Updating jamulus, sonobus & jacktrip in 5 seconds'
+echo 'Updating jamulus, sonobus, jacktrip & jamtrip in 5 seconds'
 sleep 5
 sudo apt-get update
-sudo apt-get install jamulus sonobus jacktrip
+sudo apt-get -y install jamulus sonobus jacktrip jamtrip
 sleep 20
