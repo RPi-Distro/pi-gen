d67bcfb8afe44d08705a7580514dc30d443b2859
kdoren <kevin@doren.org>
2021-01-06 19:02:29 -0800

    make jack and urlrelay conf files in /etc editable by pi user

--- stage3/99-fixup-perm/00-run.sh
+++ stage3/99-fixup-perm/00-run.sh
@@ -1,6 +1,11 @@
 on_chroot << EOF
 	chown -R ${FIRST_USER_NAME} /home/${FIRST_USER_NAME}
 	chgrp -R ${FIRST_USER_NAME} /home/${FIRST_USER_NAME}
+	# make conf files editable by pi user
+	chown root:${FIRST_USER_NAME} /etc/jackdrc.conf
+	chmod g+w /etc/jackdrc.conf
+	chown root:${FIRST_USER_NAME} /etc/urlrelay/urlrelay.conf
+	chmod g+w /etc/urlrelay/urlrelay.conf
 	if ! grep -q "swappiness" /etc/sysctl.conf; then
 		sudo echo "vm.swappiness = 10" >> /etc/sysctl.conf
 	fi
