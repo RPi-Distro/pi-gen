b94ebf2551551af0e60d436cb3c258aa472c368d
Daniel Finimundi <daniel@finimundi.com>
2021-02-21 20:14:35 -0300

    Use wlanpi default root password
    Signed-off-by: Daniel Finimundi <daniel@finimundi.com>

--- stage1/01-sys-tweaks/00-run.sh
+++ stage1/01-sys-tweaks/00-run.sh
@@ -9,7 +9,7 @@ if ! id -u ${FIRST_USER_NAME} >/dev/null 2>&1; then
 	adduser --disabled-password --gecos "" ${FIRST_USER_NAME}
 fi
 echo "${FIRST_USER_NAME}:${FIRST_USER_PASS}" | chpasswd
-echo "root:root" | chpasswd
+echo "root:Wlanpi!" | chpasswd
 EOF
 
 
