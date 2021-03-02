0ce8708daede10d479520a49e3eff434cd735dae
kdoren <kevin@doren.org>
2021-03-01 23:31:23 -0800

    simplify creation of /boot/config.txt

--- stage3/04-install-sw/08-run.sh
+++ stage3/04-install-sw/08-run.sh
@@ -8,15 +8,6 @@ cp files/libfm.conf ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/libfm/
 mkdir -p ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/lxpanel/LXDE-pi/panels
 cp files/lxde-panel ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/lxpanel/LXDE-pi/panels/panel
 
-# Configure 1024x768 screen size by default.
-sed -i -E "s/#?hdmi_group=[0-9]+/hdmi_group=2/" ${ROOTFS_DIR}/boot/config.txt
-sed -i -E "s/#?hdmi_mode=[0-9]+/hdmi_mode=16/" ${ROOTFS_DIR}/boot/config.txt
-sed -i -E "s/#?hdmi_force_hotplug=[0-9]+/hdmi_force_hotplug=1/" ${ROOTFS_DIR}/boot/config.txt
-sed -i -E "s/^dtoverlay=vc4-fkms-v3d/#dtoverlay=vc4-fkms-v3d/" ${ROOTFS_DIR}/boot/config.txt
-
-# disable wifi, we won't be using it for jamulus
-echo "dtoverlay=disable-wifi" >> ${ROOTFS_DIR}/boot/config.txt
-
-# disable bcm2835 built-in-sound
-sed -i "s/^dtparam=audio=.*/dtparam=audio=off/" ${ROOTFS_DIR}/boot/config.txt
+# copy config.txt file to /boot to set up display parameters
+cp files/config.txt ${ROOTFS_DIR}/boot/
 
