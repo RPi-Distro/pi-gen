b30cfdbdc325ea934834849a38c41744a026fac5
kdoren <kevin@doren.org>
2021-03-01 23:40:23 -0800

    reduce useless logging

--- stage3/02-misc-config2/01-run.sh
+++ stage3/02-misc-config2/01-run.sh
@@ -1,6 +1,12 @@
 install -m 644 files/cpu_performance_scaling_governor.service ${ROOTFS_DIR}/lib/systemd/system
 # install -m 644 files/rules.v4 ${ROOTFS_DIR}/etc/iptables
 
+# disable ipv6
+echo "blacklist ipv6" > ${ROOTFS_DIR}/etc/modprobe.d/ipv6.conf
+
+# Don't log router advertisement messages
+sed -i '1s/^/:msg, contains, "Router Advertisement from" stop\n\n/' ${ROOTFS_DIR}/etc/rsyslog.conf
+
 on_chroot << EOF
 	systemctl disable wifi-hotspot
 	systemctl enable cpu_performance_scaling_governor
