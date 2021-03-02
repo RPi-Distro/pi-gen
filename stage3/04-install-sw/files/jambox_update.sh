6e35d8779d076c2f3e9b5b77b3dfdc38e0f40ef2
kdoren <kevin@doren.org>
2021-03-01 23:38:01 -0800

    update app update script

--- stage3/04-install-sw/files/jambox_update.sh
+++ stage3/04-install-sw/files/jambox_update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo 'Updating jamulus, sonobus, jacktrip & jamtrip in 5 seconds'
+echo 'Updating jamming apps in 5 seconds'
 sleep 5
 sudo apt-get update
-sudo apt-get -y install jamulus sonobus jacktrip jamtrip
+sudo apt-get -y install jamulus sonobus jacktrip qjacktrip jamtaba
 sleep 20
