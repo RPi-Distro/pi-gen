0b70b405ad11b55dd412d7e6837694551ac956a0
kdoren <kevin@doren.org>
2021-03-01 23:27:34 -0800

    configure pulseaudio for realtime

--- stage3/04-install-sw/06-run.sh
+++ stage3/04-install-sw/06-run.sh
@@ -1,2 +1,6 @@
-# Set up autoamtic start of PulseAudio Jack bridge when X gets started.
+# Set up automatic start of PulseAudio Jack bridge when X gets started.
 cp files/pulseaudio-jack.desktop ${ROOTFS_DIR}/etc/xdg/autostart/
+
+# configure pulseaudio to get realtime capability
+cp files/daemon.conf ${ROOTFS_DIR}/etc/pulse/
+
