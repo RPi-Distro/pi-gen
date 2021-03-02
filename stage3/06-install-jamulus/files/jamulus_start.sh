050910c8e44600d7cbe1d20d7db3289b63bdfe6f
kdoren <kevin@doren.org>
2021-03-01 23:18:08 -0800

    update jamulus start procedure

--- stage3/06-install-jamulus/files/jamulus_start.sh
+++ stage3/06-install-jamulus/files/jamulus_start.sh
@@ -1,44 +1,28 @@
 #!/bin/bash
-
-# try to use same device as Jack by looking at jackdrc.conf
-# This can be overridden by setting JAMULUS_ALSA_DEVICE in jamulus_start.conf
-if [ -f /etc/jackdrc.conf ]; then
-  source /etc/jackdrc.conf
-  regex="^hw:([0-9]),.*"
-  if [[ "$DEVICE"  =~ $regex ]]; then
-    JAMULUS_ALSA_DEVICE="card ${BASH_REMATCH[1]}"
-  fi
-fi
+JACK_APP=jamulus
+sudo systemctl set-environment JACK_APP=jamulus
 
 if [ -f ~/.config/Jamulus/jamulus_start.conf ]; then
   source ~/.config/Jamulus/jamulus_start.conf
 fi
 
-# if $JAMULUS_ALSA_DEVICE is still not set, set a default.
-# newer kernel uses card 1 for bcm2835 Headphones,
-# in that case use card 2 for default USB audio device
-if [ -z "$JAMULUS_ALSA_DEVICE" ]; then
-  aplay -l | grep -qP "^card 1:.*Headphones"
-  if [ $? -eq 0 ]; then
-    JAMULUS_ALSA_DEVICE="card 2"
-  else
-    JAMULUS_ALSA_DEVICE="card 1"
-  fi
-fi	
+# Audio interface is chosen in /etc/jackdrc.conf
+# source it here to determine the device to use
+if [ -f /etc/jackdrc.conf ]; then
+  source /etc/jackdrc.conf
+fi
 
-[[ -z "$AJ_SNAPSHOT" ]] && AJ_SNAPSHOT="ajs-jamulus-stereo.xml"
-[[ -z "$JAMULUS_TIMEOUT" ]] && JAMULUS_TIMEOUT="120m"
-
+echo ALSA Device: $DEVICE
 ALSA_READY=no
 until [[ $ALSA_READY == "yes" ]]; do
-  aplay -l | grep -q "$JAMULUS_ALSA_DEVICE"
+  aplay -L | grep -q "$DEVICE"
   PLAY_RESULT=$?
-  arecord -l | grep -q "$JAMULUS_ALSA_DEVICE"
+  arecord -L | grep -q "$DEVICE"
   RECORD_RESULT=$?
   if [[ "$PLAY_RESULT" == "0" ]] && [[ "$RECORD_RESULT" == "0" ]]; then
     ALSA_READY=yes
   else
-    echo "ALSA Device $JAMULUS_ALSA_DEVICE not available: PLAY_RESULT: $PLAY_RESULT, RECORD_RESULT: $RECORD_RESULT"
+    echo "ALSA Device $DEVICE not available: PLAY_RESULT: $PLAY_RESULT, RECORD_RESULT: $RECORD_RESULT"
     sleep 5
   fi
 done
@@ -47,8 +31,8 @@ done
 [[ -n "$CAPTURE_LEVEL" ]] && amixer set Capture $CAPTURE_LEVEL
 
 if [ -n "$JAMULUS_SERVER" ]; then
-  # check that Jamulus server is reachable
-  while ! ping -c1 $JAMULUS_SERVER
+	# check that Jamulus server is reachable (it must have ICMP ping allowed in its firewall or security group)
+  while ! ping -c1 ${JAMULUS_SERVER%:*}
   do
     sleep 5
   done
@@ -67,20 +51,27 @@ done
 
 # Start aj-snapshot as a background process.
 # this will make the alsa/jack connections specified in snapshot file $AJ_SNAPSHOT after Jamulus starts
-if [[ -f ~/.config/aj-snapshot/$AJ_SNAPSHOT ]]; then
+if [[ -f ~/.config/aj-snapshot/${AJ_SNAPSHOT:=ajs-jamulus-stereo.xml} ]]; then
   echo "Starting aj-snapshot daemon"
   aj-snapshot --remove --daemon ~/.config/aj-snapshot/$AJ_SNAPSHOT &
+  AJ_SNAPSHOT_PID=$!
   JACKARG="--nojackconnect"
 fi
 
 # start Jamulus with --nojackconnect option if aj-snapshot is controlling the connections.
 if [ -n "$JAMULUS_SERVER" ]; then
-  timeout $JAMULUS_TIMEOUT nice -n -18 jamulus $JACKARG -c $JAMULUS_SERVER
+  timeout ${JAMULUS_TIMEOUT:-120m} chrt --${JAMULUS_SCHED:-fifo} ${JAMULUS_PRIORITY:-70} jamulus $JACKARG -c $JAMULUS_SERVER
   RESULT=$?
   # shutdown if ended due to timeout
   [[ "$RESULT" != "0" ]] && sudo shutdown now
 else
-  nice -n -18 jamulus $JACKARG
+  if [[ -n "$JAMULUS_PRIORITY" ]]; then
+    chrt --${JAMULUS_SCHED:-rr} ${JAMULUS_PRIORITY} jamulus $JACKARG
+  else
+    nice -n ${JAMULUS_NICEADJ:-0} jamulus $JACKARG
+  fi
 fi
-kill $!   # kill aj-snapshot background process
+
+[[ -n "$AJ_SNAPSHOT_PID" ]] && kill $AJ_SNAPSHOT_PID   # kill aj-snapshot background process
+sudo systemctl unset-environment JACK_APP
 exit 0
