790f943cf2baa5551f1cf4f46a6d8a226f6442ae
kdoren <kevin@doren.org>
2021-03-01 16:20:05 -0800

    add new kernels, install from repo

--- stage3/05-install-rt-kernel/00-run.sh
+++ stage3/05-install-rt-kernel/00-run.sh
@@ -1,27 +1,35 @@
-# get & extract pre-built rt kernel archive
-mkdir ${ROOTFS_DIR}/tmp/rt
-wget https://github.com/kdoren/linux/releases/download/5.4.81-rt45/kernel8-5.4.81-rt.tgz -O ${ROOTFS_DIR}/tmp/rt/kernel8-5.4.81-rt.tgz
-tar xzf ${ROOTFS_DIR}/tmp/rt/kernel8-5.4.81-rt.tgz -C ${ROOTFS_DIR}/tmp/rt/
 
-# copy kernel files to correct locations
-mkdir -p ${ROOTFS_DIR}/boot/rt/overlays
-cp -d ${ROOTFS_DIR}/tmp/rt/overlays/* ${ROOTFS_DIR}/boot/rt/overlays/
-cp -d ${ROOTFS_DIR}/tmp/rt/broadcom/bcm* ${ROOTFS_DIR}/boot/rt/
-cp ${ROOTFS_DIR}/tmp/rt/boot/kernel8_rt.img ${ROOTFS_DIR}/boot//rt/
-cp -dr ${ROOTFS_DIR}/tmp/rt/lib/* ${ROOTFS_DIR}/lib/
-rm -rf ${ROOTFS_DIR}/tmp/rt
-touch ${ROOTFS_DIR}/boot/rt/overlays/README
+install_kernel_from_deb () {
 
-# append rt kernel options to /boot/config.txt
+KERN=$1
+shift
+mkdir -p ${ROOTFS_DIR}/boot/$KERN/overlays/
+cp -d ${ROOTFS_DIR}/usr/lib/linux-image-$KERN/overlays/* ${ROOTFS_DIR}/boot/$KERN/overlays/
+cp -dr ${ROOTFS_DIR}/usr/lib/linux-image-$KERN/* ${ROOTFS_DIR}/boot/$KERN/
+touch ${ROOTFS_DIR}/boot/$KERN/overlays/README
+mv ${ROOTFS_DIR}/boot/vmlinuz-$KERN ${ROOTFS_DIR}/boot/$KERN/
+mv ${ROOTFS_DIR}/boot/System.map-$KERN ${ROOTFS_DIR}/boot/$KERN/
+cp ${ROOTFS_DIR}/boot/config-$KERN ${ROOTFS_DIR}/boot/$KERN/
+
+# append kernel options to /boot/config.txt
+while (( "$#" )); do 
 cat >> ${ROOTFS_DIR}/boot/config.txt << EOF
 
-# boot pi4 with 64-bit 5.4 realtime kernel
-[pi4]
-arm_64bit=1
-kernel=kernel8_rt.img
-os_prefix=rt/
+[$1]
+kernel=vmlinuz-$KERN
+# initramfs initrd.img-$KERN
+os_prefix=$KERN/
 overlay_prefix=overlays/
 [all]
 EOF
+shift
+done
+}
 
+
+install_kernel_from_deb "5.10.16-rt30-v7l+" "pi4"
+install_kernel_from_deb "5.10.16-rt30-v7l-usb+" "none"
+install_kernel_from_deb "5.10.17-ll-v7l+" "pi3" "pi2"
+
+# give audio group ability to raise priority with "nice"
 sed -i "s/.*audio.*nice.*$/@audio   -  nice      -19/g" ${ROOTFS_DIR}/etc/security/limits.d/audio.conf
