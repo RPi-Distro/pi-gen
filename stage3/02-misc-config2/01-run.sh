5a27c9ecb65bdb52315828348ad099329fca26cc
kdoren <kevin@doren.org>
2021-03-04 03:15:26 -0800

    use ntp for time sync

--- stage3/02-misc-config2/01-run.sh
+++ stage3/02-misc-config2/01-run.sh
@@ -11,4 +11,6 @@ on_chroot << EOF
 	systemctl disable wifi-hotspot
 	systemctl enable cpu_performance_scaling_governor
 	systemctl disable raspi-config # raspi-config is only enabling 'ondemand' governor as of 2018.08.19
+	systemctl disable systemd-timesyncd
+	systecmtl enable ntp
 EOF
