e73ce7a715cb10512cbbe3bc991560c5531a8549
Daniel Finimundi <daniel@finimundi.com>
2021-03-28 00:09:39 +0000

    Use new profiler repo
    Profiler was moved to WLAN-Pi organization.
    Also, update config and service file to be in sync
    with official repo.
    
    This is just temporary while we don't include
    profiler's debian package here.

--- wlanpi1/03-pipx/01-run.sh
+++ wlanpi1/03-pipx/01-run.sh
@@ -12,9 +12,9 @@ on_chroot <<CHEOF
 	# Install speedtest
 	pipx install speedtest-cli
 
-	# Install profiler2 
-	pipx install git+https://github.com/wlan-pi/profiler2.git@main#egg=profiler2
+	# Install profiler
+	pipx install git+https://github.com/wlan-pi/profiler.git@main#egg=profiler
 CHEOF
 
-copy_overlay /lib/systemd/system/profiler.service -o root -g root -m 644
-copy_overlay /etc/profiler2/config.ini -o root -g root -m 644
+copy_overlay /lib/systemd/system/wlanpi-profiler.service -o root -g root -m 644
+copy_overlay /etc/wlanpi-profiler/config.ini -o root -g root -m 644
