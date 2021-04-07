2ccb42ddf9444d2f08fa9ece72b931f65af12680
Daniel Finimundi <daniel@finimundi.com>
2021-04-07 13:07:38 +0000

    Install our custom packages from our repository
    Signed-off-by: Daniel Finimundi <daniel@finimundi.com>

--- wlanpi0/00-install-packages/04-run.sh
+++ wlanpi0/00-install-packages/04-run.sh
@@ -4,6 +4,9 @@ on_chroot <<CHEOF
 	# Add Kismet repository
 	wget -O - https://www.kismetwireless.net/repos/kismet-release.gpg.key | apt-key add -
 	echo 'deb https://www.kismetwireless.net/repos/apt/release/buster buster main' | tee /etc/apt/sources.list.d/kismet.list
+	
+	# Add our own custom repository
+	echo "deb [trusted=yes] https://apt.fury.io/dfinimundi /" | tee /etc/apt/sources.list.d/wlanpi.list
 
 	apt-get update
 CHEOF
