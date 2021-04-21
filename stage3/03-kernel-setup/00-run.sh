75cc64d00da7f40c758db8aad32b080b949ce883
Fayaaz Ahmed <fayaaz.ahmed@gmail.com>
2021-04-22 00:35:29 +0100

    Use performance cpu manager instead of ondemand

--- stage3/03-kernel-setup/00-run.sh
+++ stage3/03-kernel-setup/00-run.sh
@@ -1,3 +1,7 @@
+# Add threadirqs kernel command line argument
 on_chroot << EOF
     sed -i 's/$/ threadirqs/' /boot/cmdline.txt
 EOF
+
+# Use the performance governor instead of ondemand which is default
+install -m 755 files/raspi-performance-config.sh ${ROOTFS_DIR}/etc/init.d/raspi-config
