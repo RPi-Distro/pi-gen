cccf481081612d1e2ccdd30f2cb4867daceebb01
Fayaaz Ahmed <fayaaz.ahmed@gmail.com>
2021-05-03 15:44:43 +0100

    Build with ffmpeg on for aac support

--- stage3/01-install-packages/01-run.sh
+++ stage3/01-install-packages/01-run.sh
@@ -17,7 +17,8 @@ on_chroot << EOF
     export CTEST_OUTPUT_ON_FAILURE="1"
     export QT_QPA_PLATFORM="offscreen"
     mkdir -p build && cd build
-    cmake -DKEYFINDER=OFF -DCMAKE_INSTALL_PREFIX=/usr/ -S /code -B /code/build
+    cmake -DKEYFINDER=OFF -DFFMPEG=ON -DMAD=ON -DMODPLUG=ON -DWAVPACK=ON -DBULK=ON \
+        -DCMAKE_INSTALL_PREFIX=/usr/ -S /code -B /code/build
     cmake --build /code/build --target install
     ccache -s
     cpack -G DEB
