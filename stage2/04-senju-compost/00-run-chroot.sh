0102d9ada0b9c0d567e527c97b780fe0e9587e00
mactkg <mactkg@gmail.com>
2021-05-06 12:48:17 +0900

    fix

--- stage2/04-senju-compost/00-run-chroot.sh
+++ stage2/04-senju-compost/00-run-chroot.sh
@@ -9,8 +9,10 @@ apt-get update
 # https://zenn.dev/mactkg/articles/5adc624787666c
 curl -L https://raw.githubusercontent.com/tj/n/v7.2.2/bin/n -o n
 echo "1a6492df162f66131c291260a2e9fd8e64288729 n" | sha1sum -c - || exit 1
+chmod +x n
+mkdir -p "${ROOTFS_DIR}/usr/local/bin"
 mv n "${ROOTFS_DIR}/usr/local/bin"
-N_NODE_MIRROR=https://unofficial-builds.nodejs.org/download/release/ n lts
+N_NODE_MIRROR=https://unofficial-builds.nodejs.org/download/release/ N_PREFIX=${ROOTFS_DIR}/usr/local ${ROOTFS_DIR}/usr/local/bin lts
 
 # Install Node RED
 npm install -g --unsafe-perm node-red
