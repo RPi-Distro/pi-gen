b0a0fde9d0381d580ac4cfd53b080a48c3cba594
Carl McAlwane <carlmcalwane@hotmail.co.uk>
2021-06-02 13:36:09 +0100

    Config apt to retry downloads if using proxy

--- stage0/00-configure-apt/00-run.sh
+++ stage0/00-configure-apt/00-run.sh
@@ -6,6 +6,7 @@ sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list"
 sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/raspi.list"
 
 if [ -n "$APT_PROXY" ]; then
+	install -m 644 files/52retries "${ROOTFS_DIR}/etc/apt/apt.conf.d/52retries"
 	install -m 644 files/51cache "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache"
 	sed "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache" -i -e "s|APT_PROXY|${APT_PROXY}|"
 else
