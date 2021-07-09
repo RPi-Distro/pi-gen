a20dd89717f74046056b0f18e9d58fa7243c7c2f
Alex Gonzalez <alexg@balena.io>
2021-07-09 17:00:47 +0200

    balenafin: Move to using cloudsmith as apt repository
    bintray has ceased to provide service.
    
    Change-type: patch
    Signed-off-by: Alex Gonzalez <alexg@balena.io>

--- stage1/04-balenafin/00-run.sh
+++ stage1/04-balenafin/00-run.sh
@@ -3,7 +3,7 @@
 install -m 644 files/balenafin.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 sed -i "s/%%RELEASE%%/${RELEASE}/" "${ROOTFS_DIR}/etc/apt/sources.list.d/balenafin.list"
 
-on_chroot apt-key add - < files/bintray-public.key.asc
+on_chroot apt-key add - < files/cloudsmith-public.key.asc
 on_chroot << EOF
 apt-get update
 EOF
