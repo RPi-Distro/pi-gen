7ffe8b00f34f3714faa97de7b536bcba7d407ce4
Alex Gonzalez <alexg@balena.io>
2021-07-09 17:00:11 +0200

    balenafin: Use sd8887-nxp instead of sd8887-mrvl
    NXP has released this wireless driver under a GPL license so let's use
    it instead of having a module-only package.
    
    Change-type: patch
    Signed-off-by: Alex Gonzalez <alexg@balena.io>

--- stage1/04-balenafin/01-run-chroot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-for fl in /usr/src/linux-headers-*; do
-	lh=$(basename "${fl}")
-	kv="${lh#"linux-headers-"}"
-	apt-get -y install sd8887-mrvl-modules-"${kv}"
-	depmod -a "${kv}"
-done
