b725ff1a7257904cc5db16b692c6e4513b7a412f
Josh Schmelzle <josh@joshschmelzle.com>
2021-07-28 18:00:15 -0400

    webui staging has been moved to debian package

--- wlanpi1/05-setup-services/02-run.sh
+++ wlanpi1/05-setup-services/02-run.sh
@@ -5,18 +5,18 @@
 ####################
 
 # Configure: nginx
-copy_overlay /etc/nginx/nginx.conf -o root -g root -m 644
+# copy_overlay /etc/nginx/nginx.conf -o root -g root -m 644
 
 # Configure: PHP
-copy_overlay /etc/php/7.3/fpm/php.ini -o root -g root -m 644
+# copy_overlay /etc/php/7.3/fpm/php.ini -o root -g root -m 644
 
 on_chroot <<CHEOF
 	# Installing: WebUI
-	pipx install --include-deps git+https://github.com/wlan-pi/wlanpi-webui@main#egg=wlanpi_webui
+	# pipx install --include-deps git+https://github.com/wlan-pi/wlanpi-webui@main#egg=wlanpi_webui
 
 	# Remove default config: nginx
-	rm -f /etc/nginx/sites-enabled/default
+	# rm -f /etc/nginx/sites-enabled/default
 
 	# Fix permissions: /var/www
-	chown -R www-data:www-data /var/www
+	# chown -R www-data:www-data /var/www
 CHEOF
