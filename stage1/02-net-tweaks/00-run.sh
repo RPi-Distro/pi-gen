b23d0b144df1aeb87814b7ea84f27eaa30392955
Serge Schneider <serge@raspberrypi.com>
2021-10-05 15:22:16 +0100

    Use raspi-config to disable predictable network interface names

--- stage1/02-net-tweaks/00-run.sh
+++ stage1/02-net-tweaks/00-run.sh
@@ -3,4 +3,6 @@
 echo "${TARGET_HOSTNAME}" > "${ROOTFS_DIR}/etc/hostname"
 echo "127.0.1.1		${TARGET_HOSTNAME}" >> "${ROOTFS_DIR}/etc/hosts"
 
-ln -sf /dev/null "${ROOTFS_DIR}/etc/systemd/network/99-default.link"
+on_chroot << EOF
+	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_net_names 1
+EOF
