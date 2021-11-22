f390823b8a91760503b84bbf478710937466278a
jolla <jerryolla@gmail.com>
2021-11-22 12:39:18 -0600

    Update 04-run.sh
    Remove gemfury repo as it is no longer needed for RPi images

--- wlanpi0/00-install-packages/04-run.sh
+++ wlanpi0/00-install-packages/04-run.sh
@@ -5,9 +5,6 @@ on_chroot <<CHEOF
 	wget -O - https://www.kismetwireless.net/repos/kismet-release.gpg.key | apt-key add -
 	echo 'deb https://www.kismetwireless.net/repos/apt/release/buster buster main' | tee /etc/apt/sources.list.d/kismet.list
 	
-	# Add our own custom repository
-	echo "deb [trusted=yes] https://apt.fury.io/dfinimundi /" | tee /etc/apt/sources.list.d/wlanpi.list
-	
 	# Add packagecloud wlanpi/main repository
 	curl -s https://packagecloud.io/install/repositories/wlanpi/main/script.deb.sh | sudo bash
 
