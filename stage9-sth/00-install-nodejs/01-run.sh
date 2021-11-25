71884c23e071c98be7b1b2931cb1ccb5ae5f73b4
Ubuntu <ubuntu@ns3122906.ip-51-68-152.eu>
2021-11-25 21:31:11 +0000

    Add docker remove NOOBS

--- stage9-sth/00-install-nodejs/01-run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-curl -fsSL https://deb.nodesource.com/setup_lts.x -O files/setup_lts.x
-install -m 755 files/setup_lts.x	"${ROOTFS_DIR}/tmp/"
-
-on_chroot << EOF
-bash /tmp/setup_lts.x
-apt-get install -y nodejs
-EOF
