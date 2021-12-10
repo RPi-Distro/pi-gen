8ba1d7109bf449bc93daf4a31291f0d5b7a14393
jolla <jerryolla@gmail.com>
2021-12-09 20:44:52 -0600

    Update 04-run.sh
    Switching repo to bullseye

--- wlanpi0/00-install-packages/04-run.sh
+++ wlanpi0/00-install-packages/04-run.sh
@@ -3,7 +3,7 @@
 on_chroot <<CHEOF
 	# Add Kismet repository
 	wget -O - https://www.kismetwireless.net/repos/kismet-release.gpg.key | apt-key add -
-	echo 'deb https://www.kismetwireless.net/repos/apt/release/buster buster main' | tee /etc/apt/sources.list.d/kismet.list
+	echo 'deb https://www.kismetwireless.net/repos/apt/release/bullseye bullseye main' | tee /etc/apt/sources.list.d/kismet.list
 	
 	# Add packagecloud wlanpi/main repository
 	curl -s https://packagecloud.io/install/repositories/wlanpi/main/script.deb.sh | sudo bash
