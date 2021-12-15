9bc524b48bc110c46df6f866d0e5159141b600ef
jolla <jerryolla@gmail.com>
2021-12-15 03:06:04 -0600

    Update 01-run.sh
    Updating resolve.conf symlink to resolve issue with DNS

--- export-image/02-network/01-run.sh
+++ export-image/02-network/01-run.sh
@@ -1,8 +1,3 @@
 #!/bin/bash -e
 
-#install -m 644 files/resolv.conf "${ROOTFS_DIR}/etc/"
-
-on_chroot <<CHEOF
-    # Enable dynamically assigned DNS nameservers
-    ln -sf /etc/resolvconf/run/resolv.conf /etc/resolv.conf
-CHEOF
+install -m 644 files/resolv.conf "${ROOTFS_DIR}/etc/"
