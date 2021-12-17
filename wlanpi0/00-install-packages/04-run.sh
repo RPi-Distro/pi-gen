efae213ca3eaab0e5d56d159c39b80a243a6a73c
jolla <jerryolla@gmail.com>
2021-12-17 17:02:57 -0600

    Update 04-run.sh
    Added Bullseye Backport repo

--- wlanpi0/00-install-packages/04-run.sh
+++ wlanpi0/00-install-packages/04-run.sh
@@ -8,5 +8,9 @@ on_chroot <<CHEOF
 	# Add packagecloud wlanpi/main repository
 	curl -s https://packagecloud.io/install/repositories/wlanpi/main/script.deb.sh | sudo bash
 
+	# Add Bullseye Backports repository
+	echo 'deb http://deb.debian.org/debian bullseye-backports main' | tee /etc/apt/sources.list.d/bullseye-backports.list
+	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
+
 	apt-get update
 CHEOF
