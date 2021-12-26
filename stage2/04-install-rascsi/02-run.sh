90f975f292124344f2dc4df0882fe30d0822bef9
akuker <34318535+akuker@users.noreply.github.com>
2021-12-26 16:59:21 -0600

    Pull all disk images from dropbox instead of github

--- stage2/04-install-rascsi/02-run.sh
+++ stage2/04-install-rascsi/02-run.sh
@@ -3,10 +3,9 @@
 on_chroot << EOF
 su pi
 cd /home/pi/images
-# wget https://github.com/akuker/rascsi-bootstrap-images/raw/main/RaSCSI-BootstrapV2.hda_.zip
-wget https://github.com/akuker/rascsi-bootstrap-images/raw/main/RaSCSI-Boot-6.0.8.hda_.zip
-wget https://github.com/akuker/rascsi-bootstrap-images/raw/main/RaSCSI-Boot-7.0.1.hda_.zip
-wget https://github.com/akuker/rascsi-bootstrap-images/raw/main/RaSCSI-Boot-7.5.3.hda_.zip
-# wget https://github.com/akuker/rascsi-bootstrap-images/raw/main/DaynaPORT7.5.3.sit_.hqx
+wget https://www.dropbox.com/s/2ntw4h1mrdg1mhv/RaSCSI-Boot-6.0.8.hda_.zip
+wget https://www.dropbox.com/s/7s6skt71xblmt2a/RaSCSI-Boot-7.0.1.hda_.zip
+wget https://www.dropbox.com/s/u2w4hjdvdife2ts/RaSCSI-Boot-7.5.3.hda_.zip
+wget https://www.dropbox.com/s/q3h8fi54f8ry2rq/RaSCSI-Boot-8.hda_.zip
 wget https://www.dropbox.com/s/wihtro6fwfze2nm/RaSCSI-BootstrapV3.hda_.zip
 EOF
