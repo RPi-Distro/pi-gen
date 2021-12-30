624f1505f9dff6d4016297bce1e96d30f93495ab
jolla <jerryolla@gmail.com>
2021-12-30 16:41:35 -0600

    Update 03-run.sh
    Move usb0 DHCP server config to systemd/networkd

--- wlanpi1/05-setup-services/03-run.sh
+++ wlanpi1/05-setup-services/03-run.sh
@@ -14,18 +14,18 @@ on_chroot <<CHEOF
 	echo "options g_ether host_addr=5e:a4:f0:3e:31:d3 use_eem=0" > /etc/modprobe.d/g_ether.conf
 
 	# Configure DHCP: dhcpd.conf
-	cat <<-EOF >> /etc/dhcp/dhcpd.conf
+#	cat <<-EOF >> /etc/dhcp/dhcpd.conf
 
 # usb0 DHCP scope
-subnet 169.254.42.0 netmask 255.255.255.224 {
-	interface usb0;
-	range 169.254.42.2 169.254.42.30;
-	option domain-name-servers wlanpi.local;
-	option domain-name "wlanpi.local";
-	option broadcast-address 169.254.42.31;
-	default-lease-time 86400;
-	max-lease-time 86400;
-}
-EOF
+#subnet 169.254.42.0 netmask 255.255.255.224 {
+#	interface usb0;
+#	range 169.254.42.2 169.254.42.30;
+#	option domain-name-servers wlanpi.local;
+#	option domain-name "wlanpi.local";
+#	option broadcast-address 169.254.42.31;
+#	default-lease-time 86400;
+#	max-lease-time 86400;
+#}
+#EOF
 
 CHEOF
