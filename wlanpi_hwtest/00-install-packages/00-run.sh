acbefaf5b8eeb7ccf9ceceb0b30d876d28a6fa0f
Daniel Finimundi <daniel@finimundi.com>
2022-01-03 03:27:46 +0000

    No need to add repos again
    Problem was that prerun.sh was not executable, so build system was not
    copying the filesystem from previous stage.
    
    Signed-off-by: Daniel Finimundi <daniel@finimundi.com>

--- wlanpi_hwtest/00-install-packages/00-run.sh
+++ wlanpi_hwtest/00-install-packages/00-run.sh
@@ -2,14 +2,6 @@
 
 on_chroot <<CHEOF
 
-	# Add packagecloud wlanpi/main repository
-	curl -s https://packagecloud.io/install/repositories/wlanpi/main/script.deb.sh | sudo bash
-	
-	# Add packagecloud wlanpi/dev repository
-	curl -s https://packagecloud.io/install/repositories/wlanpi/dev/script.deb.sh | sudo bash	
-
-	apt-get update
-
 	# Disable FPMS as it will conflifct with wlanpi-hwtest
 	sudo systemctl disable wlanpi-fpms
 
