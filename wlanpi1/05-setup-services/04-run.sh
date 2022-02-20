e6051d4046fabe48d949f8e405d25d3a5fce007d
Daniel Finimundi <daniel@finimundi.com>
2022-02-20 02:37:01 +0000

    Revert "change iwlwifi param amsdu_size=3"
    This reverts commit e41b63978aa2d9375003be0fad589fe20334f8eb.
    
    Modification done on wlanpi-linux-firmware package to allow OTA upgrade:
    https://github.com/WLAN-Pi/wlanpi-misc-packages/pull/14

--- wlanpi1/05-setup-services/04-run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-#######################
-# Fix buffer on AX210 #
-#######################
-
-on_chroot <<CHEOF
-	echo "options iwlwifi amsdu_size=3" > /etc/modprobe.d/iwlwifi.conf
-CHEOF
