4e27d2b5f1c4e714fde2358a873e34b7bde5c08c
Josh Schmelzle <josh@joshschmelzle.com>
2022-03-02 14:35:26 -0500

    disable creation of wait.conf with do_boot_wait 1

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -4,7 +4,7 @@ install -v -d					"${ROOTFS_DIR}/etc/wpa_supplicant"
 install -v -m 600 files/wpa_supplicant.conf	"${ROOTFS_DIR}/etc/wpa_supplicant/"
 
 on_chroot << EOF
-	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_wait 0
+	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_wait 1
 EOF
 
 if [ -v WPA_COUNTRY ]; then
