18f8408c99d4788b81c43d03ad9cced986c75a10
Jiri Brejcha <jirka@jiribrejcha.net>
2022-08-24 20:43:05 +0100

    Temporarily add packagecloud wlanpi/dev repository

--- wlanpi0/00-install-packages/04-run.sh
+++ wlanpi0/00-install-packages/04-run.sh
@@ -8,6 +8,9 @@ on_chroot <<CHEOF
 	# Add packagecloud wlanpi/main repository
 	curl -s https://packagecloud.io/install/repositories/wlanpi/main/script.deb.sh | bash
 
+	# Temporarily add packagecloud wlanpi/dev repository
+	curl -s https://packagecloud.io/install/repositories/wlanpi/dev/script.deb.sh | bash
+
 	# Add Bullseye Backports repository
 	echo 'deb http://deb.debian.org/debian bullseye-backports main' | tee /etc/apt/sources.list.d/bullseye-backports.list
 	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
