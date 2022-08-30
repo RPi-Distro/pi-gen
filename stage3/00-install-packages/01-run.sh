69cd4101622c206718f25d2d39e2a2be4e4c7aba
Serge Schneider <serge@raspberrypi.com>
2022-08-30 16:54:09 +0100

    Install picamera2

--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -5,4 +5,5 @@ update-alternatives --install /usr/bin/x-www-browser \
   x-www-browser /usr/bin/chromium-browser 86
 update-alternatives --install /usr/bin/gnome-www-browser \
   gnome-www-browser /usr/bin/chromium-browser 86
+  apt-mark auto python3-pyqt5 python3-opengl
 EOF
