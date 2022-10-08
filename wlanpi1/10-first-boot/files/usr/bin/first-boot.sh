adfa334eb75393d930dc72f1e98978604c8e0701
Jiri Brejcha <jirka@jiribrejcha.net>
2022-10-08 20:39:16 +0100

    Random commit to give CI a nudge

--- wlanpi1/10-first-boot/files/usr/bin/first-boot.sh
+++ wlanpi1/10-first-boot/files/usr/bin/first-boot.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# Enable firewall
 ufw enable
 
 # Remove speedtest symlink (speedtest-cli is still available) and free it up for the official Ookla's speedtest tool
 unlink /opt/wlanpi/pipx/bin/speedtest
 unlink /usr/local/bin/speedtest
 
+# Don't autostart first boot service
 systemctl disable wlanpi-first-boot
