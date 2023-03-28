64d01d5433ae237bdf6b419de518dd2627b6b2c3
Tony Kuker <34318535+akuker@users.noreply.github.com>
2023-03-28 09:29:02 -0500

    Update paths for file downloads

--- stage2/04-install-piscsi/02-run.sh
+++ stage2/04-install-piscsi/02-run.sh
@@ -3,9 +3,9 @@
 on_chroot << EOF
 su pi
 cd /home/pi/images
-wget https://www.dropbox.com/s/i23h12gzd9a0ka3/PiSCSI-Boot-6.0.8.hda_.zip
-wget https://www.dropbox.com/s/v65129yt7b1iulq/PiSCSI-Boot-7.0.1.hda_.zip
-wget https://www.dropbox.com/s/7z8ffuitabn4ujd/PiSCSI-Boot-7.5.3.hda_.zip
-wget https://www.dropbox.com/s/pgfv66pzzkwihi1/PiSCSI-Boot-8.hda_.zip
-wget https://www.dropbox.com/s/v9dvze0g2ei77oo/PiSCSI-BootstrapV3.hda_.zip
+wget https://macintoshgarden.org/sites/macintoshgarden.org/files/apps/RaSCSI-Boot-6.0.8.hda__1.zip
+wget https://macintoshgarden.org/sites/macintoshgarden.org/files/apps/RaSCSI-Boot-7.0.1.hda__0.zip
+wget https://macintoshgarden.org/sites/macintoshgarden.org/files/apps/RaSCSI-Boot-7.5.3.hda__2.zip
+wget https://macintoshgarden.org/sites/macintoshgarden.org/files/apps/RaSCSI-Boot-8.hda__0.zip
+wget https://macintoshgarden.org/sites/macintoshgarden.org/files/apps/RaSCSI_v3_Bootstrap.hda_.zip 
 EOF
