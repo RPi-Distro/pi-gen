2fcb45f244b3700a17770ae55b191f479efe464f
Jiri Brejcha <jirka@jiribrejcha.net>
2023-05-25 07:47:34 +0100

    Adds Grafana repository

--- wlanpi0/00-install-packages/04-run.sh
+++ wlanpi0/00-install-packages/04-run.sh
@@ -15,5 +15,13 @@ on_chroot <<CHEOF
 	echo 'deb http://deb.debian.org/debian bullseye-backports main' | tee /etc/apt/sources.list.d/bullseye-backports.list
 	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
 
+	# Add Grafana repository
+	if [ ! -f /etc/apt/sources.list.d/grafana.list ]; then
+		echo "Adding grafana repository"
+		wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+		echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+		sudo apt update >/dev/null
+	fi
+
 	apt-get update
 CHEOF
