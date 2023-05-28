4ee14c56f83fd57d3842e8db6e45157ceff17304
mqjasper <62457579+mqjasper@users.noreply.github.com>
2023-05-28 12:49:13 +1000

    Added a custom MOTD to the image

--- mqstage/01-ap-config/00-run.sh
+++ mqstage/01-ap-config/00-run.sh
@@ -9,6 +9,8 @@ install -m 644 files/shui/shui.service "${ROOTFS_DIR}/lib/systemd/system/"
 # Copy preconfigured NetworkManager access point to directory
 install -m 600 files/WiFiAP.nmconnection "${ROOTFS_DIR}/etc/NetworkManager/system-connections/"
 install -m 777 files/first-boot-rename "${ROOTFS_DIR}/etc/init.d/"
+# Custom MOTD when logging in. Will provide IP address, shui status, system uptime, journalctl command
+install -m 777 files/motd "${ROOTFS_DIR}/home/pi/"
 # Using the code from raspi-config to enable NetworkManager and sense hat service. Also enabling first-boot program that refreshes SSID
 on_chroot << EOF
 update-rc.d first-boot-rename defaults
@@ -17,4 +19,6 @@ systemctl -q stop dhcpcd 2> /dev/null
 systemctl -q disable dhcpcd
 systemctl -q enable NetworkManager
 sudo dpkg --remove --force-depends raspi-config
+sudo rm /etc/motd
+cat /home/pi/motd | tee --append /home/pi/.bashrc
 EOF
