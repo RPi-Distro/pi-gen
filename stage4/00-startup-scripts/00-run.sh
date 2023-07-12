4f650e6dbac08461436d2154ddbacc62ca148381
JNorthway <jnorthway@sierrawireless.com>
2023-07-12 11:56:51 -0700

    Add check-in script service

--- stage4/00-startup-scripts/00-run.sh
+++ stage4/00-startup-scripts/00-run.sh
@@ -4,6 +4,7 @@ mkdir "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/scripts"
 install -m 700 files/get-config-script "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/scripts/"
 install -m 644 files/farm-environment "${ROOTFS_DIR}/etc/"
 install -m 644 files/hostname-gen.service "${ROOTFS_DIR}/etc/systemd/system/"
+install -m 644 files/check-in.service "${ROOTFS_DIR}/etc/systemd/system/"
 
 chown 1000:1000 "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/scripts/get-config-script"
 chown 1000:1000 "${ROOTFS_DIR}/etc/farm-environment"
@@ -11,6 +12,7 @@ chown 1000:1000 "${ROOTFS_DIR}/etc/farm-environment"
 sed -i 's:CONF_SWAPSIZE=.*:CONF_SWAPSIZE=2048:g' "${ROOTFS_DIR}/etc/dphys-swapfile"
 
 ln -sv "/etc/systemd/system/hostname-gen.service" "$ROOTFS_DIR/etc/systemd/system/multi-user.target.wants/hostname-gen.service"
+ln -sv "/etc/systemd/system/check-in.service" "$ROOTFS_DIR/etc/systemd/system/multi-user.target.wants/check-in.service"
 ln -sv "/usr/lib/systemd/system/vncserver-x11-serviced.service" "$ROOTFS_DIR/etc/systemd/system/multi-user.target.wants/vncserver-x11-serviced.service"
 
 pushd "${WORK_DIR}" > /dev/null
