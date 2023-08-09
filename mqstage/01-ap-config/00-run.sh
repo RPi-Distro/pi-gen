3633228a5bb6441e37f6381d861dd28fdcec5c77
mqjasper <jasper.catley@students.mq.edu.au>
2023-08-09 05:02:48 +0000

    Disabling the first-boot-rename service, ensuring stem_club works Disabling the service on the pi as it will require an extra reboot before it is possible to set the ssid via boot config, defeating the whole point of the new setup. Have also set the stem_club repository to clone an old version as new changes have broken current setup method, this will be fixed soon

--- mqstage/01-ap-config/00-run.sh
+++ mqstage/01-ap-config/00-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 # Clone the latest version of the stem_club repository
 cd files/ && rm -r stem_club && git clone https://github.com/altmattr/stem_club.git
+cd stem_club && git checkout 6a94a0bd4900ef74122be405d9af21a37ea7837a && cd ..
 cd ..
 sudo cp -a files/stem_club/shui "${ROOTFS_DIR}/home/pi/shui/"
 sudo cp -a files/stem_club/picam_predict "${ROOTFS_DIR}/home/pi/picam_predict/"
@@ -9,20 +10,21 @@ install -m 644 files/shui/shui.service "${ROOTFS_DIR}/lib/systemd/system/"
 # Copy preconfigured NetworkManager access point config to directory
 install -m 600 files/*.nmconnection "${ROOTFS_DIR}/etc/NetworkManager/system-connections/"
 # Load the first-boot service to randomise the SSID
-install -m 777 files/first-boot-rename "${ROOTFS_DIR}/etc/init.d/"
+# Disabling this as it doesn't seem necessary. Not gonna remove entirely just in case. Make sure to reenable rcd update if enabling.
+#install -m 777 files/first-boot-rename "${ROOTFS_DIR}/etc/init.d/"
 # Load the service to rename the SSID on boot
 install -m 777 files/rename-on-boot "${ROOTFS_DIR}/etc/init.d/"
 # Custom MOTD when logging in. Will provide IP address, shui status, system uptime, journalctl command
 install -m 777 files/motd "${ROOTFS_DIR}/home/pi/"
 # Using the code from raspi-config to enable NetworkManager, also sense hat, first-boot program, custom MOTD
 on_chroot << EOF
-sudo update-rc.d first-boot-rename defaults
+#sudo update-rc.d first-boot-rename defaults
 update-rc.d rename-on-boot defaults
 systemctl enable shui.service
 systemctl -q stop dhcpcd 2> /dev/null
 systemctl -q disable dhcpcd
 systemctl -q enable NetworkManager
-sudo rm /etc/motd
+#sudo rm /etc/motd
 cat /home/pi/motd | tee --append /home/pi/.bashrc
 rm /home/pi/motd
 sudo chmod +x /home/pi/shui/*.py
