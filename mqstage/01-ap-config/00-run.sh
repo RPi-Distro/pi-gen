51b4f29887a824821ccfcbd9209ea869bf3b254e
mqjasper <jasper.catley@students.mq.edu.au>
2023-08-11 02:47:26 +0000

    committing files

--- mqstage/01-ap-config/00-run.sh
+++ mqstage/01-ap-config/00-run.sh
@@ -6,7 +6,7 @@ cd ..
 sudo cp -a files/stem_club/shui "${ROOTFS_DIR}/home/pi/shui/"
 sudo cp -a files/stem_club/picam_predict "${ROOTFS_DIR}/home/pi/picam_predict/"
 # Install the service that runs the sense hat program at boot 
-install -m 644 files/shui/shui.service "${ROOTFS_DIR}/lib/systemd/system/"
+install -m 644 "${ROOTFS_DIR}/home/pi/shui/shui.service" "${ROOTFS_DIR}/lib/systemd/system/"
 # Copy preconfigured NetworkManager access point config to directory
 install -m 600 files/*.nmconnection "${ROOTFS_DIR}/etc/NetworkManager/system-connections/"
 # Load the first-boot service to randomise the SSID
@@ -17,14 +17,13 @@ install -m 777 files/rename-on-boot "${ROOTFS_DIR}/etc/init.d/"
 # Custom MOTD when logging in. Will provide IP address, shui status, system uptime, journalctl command
 install -m 777 files/motd "${ROOTFS_DIR}/home/pi/"
 # Using the code from raspi-config to enable NetworkManager, also sense hat, first-boot program, custom MOTD
-on_chroot << EOF
 #sudo update-rc.d first-boot-rename defaults
+on_chroot << EOF
 update-rc.d rename-on-boot defaults
 systemctl enable shui.service
 systemctl -q stop dhcpcd 2> /dev/null
 systemctl -q disable dhcpcd
 systemctl -q enable NetworkManager
-#sudo rm /etc/motd
 cat /home/pi/motd | tee --append /home/pi/.bashrc
 rm /home/pi/motd
 sudo chmod +x /home/pi/shui/*.py
--- mqstage/01-ap-config/files/shui/capture.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-raspistill -tl 1000 -t 0 -rot 180 -o /home/pi/captures/img%04d.jpg
