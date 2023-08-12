09a26a48c0bd1ca66148efc3ea993dc766f6bbed
mqjasper <jasper.catley@students.mq.edu.au>
2023-08-12 17:47:30 +1000

    add hostname, fix stem_club implementation

--- mqstage/01-ap-config/00-run.sh
+++ mqstage/01-ap-config/00-run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -e
 # Clone the latest version of the stem_club repository
 cd files/ && rm -r stem_club && git clone https://github.com/altmattr/stem_club.git
-cd stem_club && git checkout 6a94a0bd4900ef74122be405d9af21a37ea7837a && cd ..
 cd ..
-sudo cp -a files/stem_club/shui "${ROOTFS_DIR}/home/pi/shui/"
-sudo cp -a files/stem_club/picam_predict "${ROOTFS_DIR}/home/pi/picam_predict/"
-# Install the service that runs the sense hat program at boot 
-install -m 644 "${ROOTFS_DIR}/home/pi/shui/shui.service" "${ROOTFS_DIR}/lib/systemd/system/"
+sudo cp -a files/stem_club/shui "${ROOTFS_DIR}/home/pi/stem_club/"
+# Install all the services included in the stem_club repository. Only the interface is enabled from boot. 
+install -m 644 "${ROOTFS_DIR}/home/pi/stem_club/services/interface.service" "${ROOTFS_DIR}/lib/systemd/system/"
+install -m 644 "${ROOTFS_DIR}/home/pi/stem_club/services/prediction.service" "${ROOTFS_DIR}/lib/systemd/system/"
+install -m 644 "${ROOTFS_DIR}/home/pi/stem_club/services/sensors.service" "${ROOTFS_DIR}/lib/systemd/system/"
 # Copy preconfigured NetworkManager access point config to directory
 install -m 600 files/*.nmconnection "${ROOTFS_DIR}/etc/NetworkManager/system-connections/"
 # Load the first-boot service to randomise the SSID
@@ -18,16 +18,17 @@ install -m 777 files/rename-on-boot "${ROOTFS_DIR}/etc/init.d/"
 install -m 777 files/motd "${ROOTFS_DIR}/home/pi/"
 # Using the code from raspi-config to enable NetworkManager, also sense hat, first-boot program, custom MOTD
 #sudo update-rc.d first-boot-rename defaults
+# sudo chmod +x /home/pi/shui/*.py
+# sudo chmod +x /home/pi/shui/*.sh
+# sudo chmod +x /home/pi/picam_predict/*.py
 on_chroot << EOF
 update-rc.d rename-on-boot defaults
-systemctl enable shui.service
+systemctl enable interface.service
 systemctl -q stop dhcpcd 2> /dev/null
 systemctl -q disable dhcpcd
 systemctl -q enable NetworkManager
 cat /home/pi/motd | tee --append /home/pi/.bashrc
 rm /home/pi/motd
-sudo chmod +x /home/pi/shui/*.py
-sudo chmod +x /home/pi/shui/*.sh
-sudo chmod +x /home/pi/picam_predict/*.py
+
 sudo chmod -R 777 /home/pi/
 EOF
