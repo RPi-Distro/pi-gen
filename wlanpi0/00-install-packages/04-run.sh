af29012f6ea039669dea304045d8d42c54c7a862
Jiri Brejcha <jirka@jiribrejcha.net>
2023-08-24 12:50:49 +0100

    Add tshoot output

--- wlanpi0/00-install-packages/04-run.sh
+++ wlanpi0/00-install-packages/04-run.sh
@@ -28,6 +28,10 @@ on_chroot <<CHEOF
 		curl https://repos.influxdata.com/influxdata-archive.key | gpg --dearmor | sudo tee /usr/share/keyrings/influxdb-archive-keyring.gpg >/dev/null
 		echo "deb [signed-by=/usr/share/keyrings/influxdb-archive-keyring.gpg] https://repos.influxdata.com/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
 	fi
+ 	
+  	echo "--tshoot"
+	cat /etc/apt/sources.list.d/influxdb.list
+	echo "--tshoot"
 
 	echo "Running apt update"
 	apt update
