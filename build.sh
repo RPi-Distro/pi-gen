8dd23cbc1d01b2a89f560c98f5e5b8ef2e71fbf3
Serge Schneider <serge@raspberrypi.com>
2023-09-07 13:34:21 +0100

    Remove dhcpcd and associated options

--- build.sh
+++ build.sh
@@ -227,8 +227,6 @@ export FIRST_USER_NAME=${FIRST_USER_NAME:-pi}
 export FIRST_USER_PASS
 export DISABLE_FIRST_BOOT_USER_RENAME=${DISABLE_FIRST_BOOT_USER_RENAME:-0}
 export RELEASE=${RELEASE:-bookworm} # Don't forget to update stage0/prerun.sh
-export WPA_ESSID
-export WPA_PASSWORD
 export WPA_COUNTRY
 export ENABLE_SSH="${ENABLE_SSH:-0}"
 export PUBKEY_ONLY_SSH="${PUBKEY_ONLY_SSH:-0}"
