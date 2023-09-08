188c155717b68dbd5704f337fd3b2cbd1b80d966
Serge Schneider <serge@raspberrypi.com>
2023-09-08 11:39:51 +0100

    Migrate to raspberrypi.com

--- stage0/prerun.sh
+++ stage0/prerun.sh
@@ -6,5 +6,5 @@ if [ "$RELEASE" != "bookworm" ]; then
 fi
 
 if [ ! -d "${ROOTFS_DIR}" ] || [ "${USE_QCOW2}" = "1" ]; then
-	bootstrap ${RELEASE} "${ROOTFS_DIR}" http://raspbian.raspberrypi.org/raspbian/
+	bootstrap ${RELEASE} "${ROOTFS_DIR}" http://raspbian.raspberrypi.com/raspbian/
 fi
