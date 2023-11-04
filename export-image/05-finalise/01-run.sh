ae103f7deaf2e3e096b8a42d854f62779734077f
Fayaaz Ahmed <fayaaz.ahmed@censornet.com>
2023-11-04 23:48:42 +0000

    More updates to build scripts

--- export-image/05-finalise/01-run.sh
+++ export-image/05-finalise/01-run.sh
@@ -81,16 +81,41 @@ cp "$ROOTFS_DIR/opt/mixxx.version" "$MIXXX_VERSION_FILE"
 } >> "$INFO_FILE"
 
 mkdir -p "${DEPLOY_DIR}"
-rm -f "${DEPLOY_DIR}/${ZIP_FILENAME}${IMG_SUFFIX}.zip"
+rm -f "${DEPLOY_DIR}/${ARCHIVE_FILENAME}${IMG_SUFFIX}.*"
 rm -f "${DEPLOY_DIR}/${IMG_FILENAME}${IMG_SUFFIX}.img"
 
-if [ "${DEPLOY_ZIP}" == "1" ]; then
-	pushd "${STAGE_WORK_DIR}" > /dev/null
-	zip "${DEPLOY_DIR}/${ZIP_FILENAME}${IMG_SUFFIX}.zip" \
-		"$(basename "${IMG_FILE}")"
-	popd > /dev/null
+
+mv "$INFO_FILE" "$DEPLOY_DIR/"
+
+if [ "${USE_QCOW2}" = "0" ] && [ "${NO_PRERUN_QCOW2}" = "0" ]; then
+	ROOT_DEV="$(mount | grep "${ROOTFS_DIR} " | cut -f1 -d' ')"
+
+	unmount "${ROOTFS_DIR}"
+	zerofree "${ROOT_DEV}"
+
+	unmount_image "${IMG_FILE}"
 else
 	cp "$IMG_FILE" "$DEPLOY_DIR"
 fi
+
+case "${DEPLOY_COMPRESSION}" in
+zip)
+	pushd "${STAGE_WORK_DIR}" > /dev/null
+	zip -"${COMPRESSION_LEVEL}" \
+	"${DEPLOY_DIR}/${ARCHIVE_FILENAME}${IMG_SUFFIX}.zip" "$(basename "${IMG_FILE}")"
+	popd > /dev/null
+	;;
+gz)
+	pigz --force -"${COMPRESSION_LEVEL}" "$IMG_FILE" --stdout > \
+	"${DEPLOY_DIR}/${ARCHIVE_FILENAME}${IMG_SUFFIX}.img.gz"
+	;;
+xz)
+	xz --compress --force --threads 0 --memlimit-compress=50% -"${COMPRESSION_LEVEL}" \
+	--stdout "$IMG_FILE" > "${DEPLOY_DIR}/${ARCHIVE_FILENAME}${IMG_SUFFIX}.img.xz"
+	;;
+none | *)
+	cp "$IMG_FILE" "$DEPLOY_DIR/"
+;;
+esac
 
 mv "$MIXXX_VERSION_FILE" "$DEPLOY_DIR"
