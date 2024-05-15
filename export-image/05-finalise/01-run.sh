d790fed832188d41c1fb5d8506d833d053e4a81c
Pev <pev@high-resolution.co.uk>
2024-05-15 15:45:43 +0100

    Compare mount output using base of dirname (#771)
    * Compare mount output using base of dirname
    
    If you're building from a bind-mounted directory, the build will
    fail as it will find two entries to unmount, but a single unmount
    will remove them both causing an error. Adding a space means that
    the mountpoint will only match with a single mount entry ; the
    expected path, rather than the pre bind-mount.
    
    * Switch to awk instead of mount, grep, cut pipes
    
    Retry unmount 5 times and give up, letting the user know that they need to resolve the issue manually
    
    ---------
    
    Co-authored-by: David Peverley <pev@analogue-micro.com>
    Co-authored-by: Serge Schneider <serge@raspberrypi.com>

--- export-image/05-finalise/01-run.sh
+++ export-image/05-finalise/01-run.sh
@@ -83,7 +83,7 @@ cp "$ROOTFS_DIR/etc/rpi-issue" "$INFO_FILE"
 	dpkg -l --root "$ROOTFS_DIR"
 } >> "$INFO_FILE"
 
-ROOT_DEV="$(mount | grep "${ROOTFS_DIR} " | cut -f1 -d' ')"
+ROOT_DEV="$(awk "\$2 == \"${ROOTFS_DIR}\" {print \$1}" /etc/mtab)"
 
 unmount "${ROOTFS_DIR}"
 zerofree "${ROOT_DEV}"
