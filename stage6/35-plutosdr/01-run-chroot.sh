7b614eaeea2099d5ce660bcf80f9f36150dc0872
Stanislav Lechev [0xAF] <af@0xAF.org>
2024-06-19 03:16:57 +0300

    updates

--- stage6/35-plutosdr/01-run-chroot.sh
+++ stage6/35-plutosdr/01-run-chroot.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -euxo pipefail
+set -euo pipefail
 
 # cmakebuild <folder> [git_tag|git_commit] [-DCMAKE_FLAG=YES ...]
 source /tmp/cmake_helper.sh
@@ -7,9 +7,6 @@ source /tmp/cmake_helper.sh
 pushd /tmp
 
 git clone --depth 1 https://github.com/pothosware/SoapyPlutoSDR.git
-echo;echo;echo;echo;echo
-echo $MAKEFLAGS
-echo;echo;echo;echo;echo
 cmakebuild SoapyPlutoSDR
 ldconfig
 
--- stage7/10-openwebrx/01-run-chroot.sh
+++ stage7/10-openwebrx/01-run-chroot.sh
@@ -3,3 +3,7 @@
 #echo "+++++ Running bash in " $(uname -m)
 #exec /usr/bin/env bash
 
+# remove and install latest acarsdec
+# TODO: find who installs the old version and where it comes from before this point
+# apt remove -y --purge acarsdec
+# apt install -y acarsdec
