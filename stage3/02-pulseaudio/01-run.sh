046024f53833e7cc9ce296c66d7924b84ea4a9a9
Mathieu Réquillart <mathieu.requillart@gmail.com>
2024-07-23 01:05:54 +0200

    fix (partial): pulseaudio, systemctl still use root instead of user

--- stage3/02-pulseaudio/01-run.sh
+++ stage3/02-pulseaudio/01-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-install -v -m files/default.pa "${ROOTFS_DIR}/etc/pulseaudio/default.pa"
+install -v -m 644 files/default.pa "${ROOTFS_DIR}/etc/pulse/default.pa"
 
 on_chroot << EOF
   SUDO_USER="${FIRST_USER_NAME}" systemctl --user enable pulseaudio
