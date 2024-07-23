1ef4b77199dc081b1e08d3fd50a91d9a43d41e20
Mathieu Réquillart <mathieu.requillart@gmail.com>
2024-07-23 20:08:15 +0200

    fix: mpd use ignore file perm

--- stage3/06-usb-sticks/00-run.sh
+++ stage3/06-usb-sticks/00-run.sh
@@ -5,7 +5,7 @@ install -v -m 755 'files/usb-mount.sh' "${ROOTFS_DIR}/usr/local/bin/usb-mount.sh
 
 install -v -m 644 'files/usb-mount@.service' "${ROOTFS_DIR}/etc/systemd/system/usb-mount@.service"
 install -v -m 644 'files/99-usb.rules' "${ROOTFS_DIR}/etc/udev/rules.d/99-usb.rules"
-install -v -m 644 'files/.mpdignore' "${ROOTFS_DIR}/media/USB/.mpdignore"
+install -v -o 1000 -g 29 -m 644 'files/.mpdignore' "${ROOTFS_DIR}/media/USB/.mpdignore"
 
 on_chroot << EOF
 #	udevadm control --reload-rules
