00f7ceb56a7c8a17c14add93ca26a883bfa6da0f
Mathieu Réquillart <mathieu.requillart@gmail.com>
2024-07-23 20:07:49 +0200

    move all mpd config to user/.config

--- stage3/04-mpd/01-run.sh
+++ stage3/04-mpd/01-run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -e
 
 install -v -o 1000 -g 29 -m 775 -d "${ROOTFS_DIR}/media/USB"
-install -v -m 644 files/mpd.conf "${ROOTFS_DIR}/etc/mpd.conf"
-sed -i "s/TARGET_HOSTNAME/${TARGET_HOSTNAME}/g" "${ROOTFS_DIR}/etc/mpd.conf"
+
+install -v -o 1000 -g 1000 -m 775 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/mpd"
+install -v -o 1000 -g 1000 -m 775 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/mpd/playlists"
+install -v -m 644 /dev/null "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/mpd/state"
+install -v -o 1000 -g 1000 -m 644 files/mpd.conf "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/mpd/mpd.conf"
+sed -i "s/TARGET_HOSTNAME/${TARGET_HOSTNAME}/g" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/mpd/mpd.conf"
 
 on_chroot << EOF
   systemctl disable mpd
