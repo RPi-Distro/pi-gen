98f522e64dd3d9f1d337f223ffa6dd8298808d9b
Serge Schneider <serge@raspberrypi.com>
2024-08-27 13:00:07 +0100

    stage3: Switch chromium package and remove unnecessary steps

--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -e
 
-on_chroot << EOF
-update-alternatives --install /usr/bin/x-www-browser \
-  x-www-browser /usr/bin/chromium-browser 86
-update-alternatives --install /usr/bin/gnome-www-browser \
-  gnome-www-browser /usr/bin/chromium-browser 86
-  apt-mark auto python3-pyqt5 python3-opengl
+on_chroot <<- EOF
+	apt-mark auto python3-pyqt5 python3-opengl
 EOF
