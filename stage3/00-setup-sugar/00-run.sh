c0f6ca96cf26005f8487a4836a5daecf3d52a947
44yu5h <meet44yu5h@gmail.com>
2024-09-20 10:59:27 +0530

    mvp test03
    Results:
    Big success!
    
    What worked at expected?
    - Packages get installed & the script runs and works as expected (i.e. makes Activity folder and git clones)
    - Image size is 1.1GB with sugar and lighDM which is expected and good.
    
    What didn't work?
    - Cannot configure lightdm
    In stage3, after installing the packages in 00-packages (including lightdm and xserver-xorg), when the script ran it said that lightdm is not installed.
    Should it be ran in chroot environment? or should it be ran in a substage of stage3?
    
    Will try both and update in the next commit

--- stage3/00-setup-sugar/00-run.sh
+++ stage3/00-setup-sugar/00-run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo raspi-config nonint do_boot_behaviour B3
 dpkg-reconfigure lightdm
-mkdir ~/Activities
-cd ~/Activities
+sudo raspi-config nonint do_boot_behaviour B3
+mkdir ${ROOTFS_DIR}/home/pi/Activities
+cd ${ROOTFS_DIR}/home/pi/Activities
 git clone https://github.com/44yu5h/gallery_activity.git
 echo "###### Finished 01-run.sh #####"
