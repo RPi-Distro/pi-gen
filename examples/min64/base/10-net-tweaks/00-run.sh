5a5ba7195ebe608caaa518734d306799c56d0b57
Matthew Lear <matthew.lear@raspberrypi.com>
2024-10-18 12:19:36 +0100

    min64: Use persistent name names

--- examples/min64/base/10-net-tweaks/00-run.sh
+++ examples/min64/base/10-net-tweaks/00-run.sh
@@ -3,13 +3,9 @@
 echo "${TARGET_HOSTNAME}" > "${ROOTFS_DIR}/etc/hostname"
 echo "127.0.1.1		${TARGET_HOSTNAME}" >> "${ROOTFS_DIR}/etc/hosts"
 
-on_chroot << EOF
-	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_net_names 1
-EOF
-
-cat << EOF > ${ROOTFS_DIR}/etc/systemd/network/01-eth0.network
+cat << EOF > ${ROOTFS_DIR}/etc/systemd/network/01-end0.network
 [Match]
-Name=eth0
+Name=end0
 
 [Network]
 DHCP=yes
