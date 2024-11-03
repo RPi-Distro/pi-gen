de673ce149e4b4b2dbd998c8fbb18070a85c81c1
Stanislav Lechev [0xAF] <af@0xAF.org>
2024-11-03 03:52:28 +0200

    fix m17 build

--- stage6/85-m17/01-run-chroot.sh
+++ stage6/85-m17/01-run-chroot.sh
@@ -6,6 +6,10 @@ source /tmp/cmake_helper.sh
 
 pushd /tmp
 
+# first install blaze
+git clone https://bitbucket.org/blaze-lib/blaze/
+cmakebuild blaze
+
 git clone https://github.com/mobilinkd/m17-cxx-demod.git
 cmakebuild m17-cxx-demod
 ldconfig
