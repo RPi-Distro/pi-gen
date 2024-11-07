e1d7107f0127e5fd73636e7996113fb9192b0506
José L. Redrejo Rodríguez <jredrejo@gmail.com>
2024-11-07 17:23:26 +0100

    Changes to work with Debian 12

--- stage2/04-hostapd/04-run.sh
+++ stage2/04-hostapd/04-run.sh
@@ -5,8 +5,12 @@ install -m 644 *.yml               "${ROOTFS_DIR}/etc/kolibri/ansible/"
 
 cp -R files                        "${ROOTFS_DIR}/etc/kolibri/ansible/"
 
-install -m 755 firstboot.sh        "${ROOTFS_DIR}/boot/"
+install -m 755 firstboot.sh        "${ROOTFS_DIR}/etc/init.d/"
 
 install -m 644 firstboot.service   "${ROOTFS_DIR}/lib/systemd/system/"
 
 ln -sf "${ROOTFS_DIR}/lib/systemd/system/firstboot.service" "${ROOTFS_DIR}/etc/systemd/system/multi-user.target.wants/firstboot.service"
+
+on_chroot << EOF
+systemctl enable firstboot
+EOF
