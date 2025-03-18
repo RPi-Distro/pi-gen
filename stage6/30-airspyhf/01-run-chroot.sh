a2381a76f24f171f4169972ad2eeeaf12690728c
Stanislav Lechev [0xAF] <af@0xAF.org>
2025-03-18 23:20:58 +0200

    fix airspy permissions

--- stage6/30-airspyhf/01-run-chroot.sh
+++ stage6/30-airspyhf/01-run-chroot.sh
@@ -15,6 +15,13 @@ git clone https://github.com/pothosware/SoapyAirspyHF.git
 cmakebuild SoapyAirspyHF
 ldconfig
 
+
+# fix airspy mini and airspy hf permissions
+cat >> /etc/udev/rules.d/52-airspy.rules << __EOF__
+SUBSYSTEM=="usb",ENV{DEVTYPE}=="usb_device",ATTRS{idVendor}=="1d50",ATTRS{idProduct}=="60a1",MODE:="0666"
+SUBSYSTEM=="usb",ENV{DEVTYPE}=="usb_device",ATTRS{idVendor}=="03eb",ATTRS{idProduct}=="800c",MODE:="0666"
+__EOF__
+
 rm -rf airspyhf SoapyAirspyHF
 
 popd
