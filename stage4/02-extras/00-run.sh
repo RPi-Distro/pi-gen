7afb2b00837edf5da95ed4ac82e39ced8c817547
paulober <44974737+paulober@users.noreply.github.com>
2025-04-08 02:13:01 +0200

    Fix wf-panel no shortcuts
    Signed-off-by: paulober <44974737+paulober@users.noreply.github.com>

--- stage4/02-extras/00-run.sh
+++ stage4/02-extras/00-run.sh
@@ -1,12 +1,21 @@
 #!/bin/bash -e
 
+# Alacarte fixes
+DIRS=(
+  ".local"
+  ".local/share"
+  ".local/share/applications"
+  ".local/share/desktop-directories"
+)
 
 if [[ "${ENABLE_CLOUD_INIT}" == "0" ]]; then
-
-#Alacarte fixes
-install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.local"
-install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.local/share"
-install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.local/share/applications"
-install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.local/share/desktop-directories"
-
+  BASE_DIR="${ROOTFS_DIR}/home/${FIRST_USER_NAME}"
+  OWNER="1000:1000"
+else
+  BASE_DIR="${ROOTFS_DIR}/etc/skel"
+  OWNER="root:root"
 fi
+
+for dir in "${DIRS[@]}"; do
+  install -v -o "${OWNER%%:*}" -g "${OWNER##*:}" -d "${BASE_DIR}/${dir}"
+done
