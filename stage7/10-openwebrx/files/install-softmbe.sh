1de001e439787144cfc050d3bf74c85b322a9af2
Stanislav Lechev [0xAF] <af@0xAF.org>
2025-04-10 18:08:23 +0300

    add new dep and update softmbe script

--- stage7/10-openwebrx/files/install-softmbe.sh
+++ stage7/10-openwebrx/files/install-softmbe.sh
@@ -10,25 +10,23 @@ apt -y install --no-install-recommends $BUILD_PACKAGES libcodecserver-dev
 pushd /tmp
 
 echo "+ Build MBELIB..."
-git clone https://github.com/szechyjs/mbelib.git
+git clone https://github.com/0xAF/mbelib
 cd mbelib
 dpkg-buildpackage
 cd ..
 rm -rf mbelib
-dpkg -i libmbe1_1.3.0_*.deb libmbe-dev_1.3.0_*.deb
+dpkg -i libmbe1_1.3*.deb libmbe-dev_1.3*.deb
 
 echo "+ Build codecserver-softmbe..."
-git clone https://github.com/knatterfunker/codecserver-softmbe.git
+git clone https://github.com/0xAF/codecserver-softmbe
 cd codecserver-softmbe
-# ignore missing library linking error in dpkg-buildpackage command
-sed -i 's/dh \$@/dh \$@ --dpkg-shlibdeps-params=--ignore-missing-info/' debian/rules
 dpkg-buildpackage
 cd ..
 rm -rf codecserver-softmbe
 
 apt remove -y --purge --autoremove $BUILD_PACKAGES
 
-dpkg -i /tmp/libmbe1_1.3.0_*.deb
+dpkg -i /tmp/libmbe1_1.3*.deb
 dpkg -i /tmp/codecserver-driver-softmbe_0.0.1_*.deb
 
 rm -f /tmp/libmbe* /tmp/codecserver-driver-softmbe*
