c088098e2acc9d312eb8f16e034dd35b75c523ad
Hugo Jacquet <hugo.jacquet@kheops.ch>
2025-04-22 17:05:56 +0200

    feat(configuration): add base config for the Biomi build

--- stage3/prerun.sh
+++ stage3/prerun.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -e
 
+# Check dev packages installation
+DEV_PACKAGES_DIRECTORY="${STAGE_WORK_DIR}/01-install-dev-packages"
+if [ "${INSTALL_DEV_PACKAGES:-0}" != "1" ]; then
+  echo "Disable dev package installation..."
+  touch "${DEV_PACKAGES_DIRECTORY}/SKIP"
+else
+  echo "Enable dev packages installation..."
+  rm -f "${DEV_PACKAGES_DIRECTORY}/SKIP" 2>/dev/null || true
+fi
+
 if [ ! -d "${ROOTFS_DIR}" ]; then
 	copy_previous
 fi
