03a0cea017e3215c5564ef6482911f92501d221e
Hugo Jacquet <hugo.jacquet@kheops.ch>
2025-04-23 10:43:23 +0200

    feat(github-actions): add github actions and fix dev dependencies installation

--- stage3/prerun.sh
+++ stage3/prerun.sh
@@ -4,6 +4,9 @@
 DEV_PACKAGES_DIRECTORY="${STAGE_WORK_DIR}/01-install-dev-packages"
 if [ "${INSTALL_DEV_PACKAGES:-0}" != "1" ]; then
   echo "Disable dev package installation..."
+
+  mkdir -p "${STAGE_WORK_DIR}/01-install-dev-packages"
+
   touch "${DEV_PACKAGES_DIRECTORY}/SKIP"
 else
   echo "Enable dev packages installation..."
