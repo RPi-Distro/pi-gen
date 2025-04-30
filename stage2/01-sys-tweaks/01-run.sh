64768e8a0a20d0f02992ad0e30b9392a132fc5cd
Ben Collins <bcollins@watter.com>
2025-04-30 00:35:27 +0000

    WCS-8: Base rpi image for watter-rpi
    Signed-off-by: Ben Collins <bcollins@watter.com>

--- stage2/01-sys-tweaks/01-run.sh
+++ stage2/01-sys-tweaks/01-run.sh
@@ -47,7 +47,7 @@ on_chroot <<EOF
 for GRP in input spi i2c gpio; do
 	groupadd -f -r "\$GRP"
 done
-for GRP in adm dialout cdrom audio users sudo video games plugdev input gpio spi i2c netdev render; do
+for GRP in adm dialout cdrom audio users sudo video games plugdev input gpio spi i2c; do
   adduser $FIRST_USER_NAME \$GRP
 done
 EOF
--- stage4/01-console-autologin/00-run.sh
+++ stage4/01-console-autologin/00-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 on_chroot << EOF
-	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_behaviour B4
+	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_behaviour B1
 EOF
