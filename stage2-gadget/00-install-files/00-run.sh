9043b1ef40de1c8d22512edf546f04ad01715149
CG <4055365+curtisgalloway@users.noreply.github.com>
2025-05-31 20:59:42 -0700

    Change ipv4 to link-local only to prevent the host from routing over it.

--- stage2-gadget/00-install-files/00-run.sh
+++ stage2-gadget/00-install-files/00-run.sh
@@ -1,4 +1,4 @@
 install -v -o root -m 644 -t "${ROOTFS_DIR}/etc/systemd/system" files/usb-gadget.service
-install -v -o root -m 600 -t "${ROOTFS_DIR}/etc/NetworkManager/system-connections" files/usb0-share files/usb1-share
+install -v -o root -m 600 -t "${ROOTFS_DIR}/etc/NetworkManager/system-connections" files/usb0-link-local files/usb1-link-local
 install -v -o root -m 644 -t "${ROOTFS_DIR}/etc/udev/rules.d" files/90-manage-usb0-gadget.rules files/90-manage-usb1-gadget.rules
 install -v -o root -m 755 -t "${ROOTFS_DIR}/usr/local/sbin" files/usb-gadget-ctl.sh
