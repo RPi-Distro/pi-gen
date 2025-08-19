59b16f3bb46160e73f029edf6ae8df3b0a4e0c67
Serge Schneider <serge@raspberrypi.com>
2025-08-19 13:07:16 +0100

    stage2: Don't modify journald.conf. This is done via a drop-in config file in sys-mods.

--- stage2/01-sys-tweaks/01-run.sh
+++ stage2/01-sys-tweaks/01-run.sh
@@ -55,8 +55,6 @@ on_chroot << EOF
 DEBIAN_FRONTEND=noninteractive dpkg-reconfigure keyboard-configuration console-setup
 EOF
 
-sed -i 's/^#\?Storage=.*/Storage=volatile/' "${ROOTFS_DIR}/etc/systemd/journald.conf"
-
 if [ -e "${ROOTFS_DIR}/etc/avahi/avahi-daemon.conf" ]; then
   sed -i 's/^#\?publish-workstation=.*/publish-workstation=yes/' "${ROOTFS_DIR}/etc/avahi/avahi-daemon.conf"
 fi
