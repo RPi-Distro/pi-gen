d5baee9b8cef62e6cd52fdeef7a42fec905293b3
Chao Fu <cfu@creaxon.com>
2025-09-04 17:36:06 +0800

    first commit

---
+++ stage3/00-dummy/01-run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+echo "Running dummy stage6 script"
+
+# This script does nothing - it's just a placeholder
+# When you're ready to add custom configurations, replace this with your actual setup
+
+echo "Dummy stage6 completed (no actions performed)"
