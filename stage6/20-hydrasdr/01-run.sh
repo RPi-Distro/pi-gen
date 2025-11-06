3a0f8842d0c876e2dbd26c5fdc5a7d0cf3e026ab
Stanislav Lechev [0xAF] <af@0xAF.org>
2025-11-07 00:04:40 +0200

    update dream package

--- stage6/20-hydrasdr/01-run.sh
+++ stage6/20-hydrasdr/01-run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -euo pipefail
-echo "blacklist dvb_usb_rtl28xxu" > "${ROOTFS_DIR}/etc/modprobe.d/rtlsdr_blacklist.conf"
+#echo "blacklist dvb_usb_rtl28xxu" > "${ROOTFS_DIR}/etc/modprobe.d/rtlsdr_blacklist.conf"
