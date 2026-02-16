4b9cd15c6db9290d5e842503464e1d3429d92f37
Serge Schneider <serge@raspberrypi.com>
2026-02-16 16:47:28 +0000

    Set FIRST_USER_NAME shell to nologin if no password is set

--- stage1/01-sys-tweaks/00-run.sh
+++ stage1/01-sys-tweaks/00-run.sh
@@ -4,11 +4,12 @@ install -v -m 644 files/fstab "${ROOTFS_DIR}/etc/fstab"
 
 on_chroot << EOF
 if ! id -u ${FIRST_USER_NAME} >/dev/null 2>&1; then
-	adduser --disabled-password --gecos "" ${FIRST_USER_NAME}
+	adduser --disabled-login --gecos "" ${FIRST_USER_NAME}
 fi
 
 if [ -n "${FIRST_USER_PASS}" ]; then
 	echo "${FIRST_USER_NAME}:${FIRST_USER_PASS}" | chpasswd
+	usermod -s /bin/bash "${FIRST_USER_NAME}"
 fi
 echo "root:root" | chpasswd
 EOF
