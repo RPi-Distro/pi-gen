1d65f915d545fac724d795a69f3799d9a07bcfa8
Serge Schneider <serge@raspberrypi.com>
2026-09-14 15:09:47 +0100

    build-docker.sh: fix shellcheck SC2140 warnings

--- build-docker.sh
+++ build-docker.sh
@@ -130,12 +130,12 @@ if [[ "${binfmt_misc_required}" == "1" ]]; then
   fi
   if ! grep -q "^interpreter ${qemu_arm}" /proc/sys/fs/binfmt_misc/qemu-arm* ; then
     # Register qemu-arm for binfmt_misc
-    reg="echo ':qemu-arm-rpi:M::"\
-"\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00:"\
-"\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:"\
-"${qemu_arm}:F' > /proc/sys/fs/binfmt_misc/register"
+    reg=':qemu-arm-rpi:M:'
+    reg+=':\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00'
+    reg+=':\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff'
+    reg+=":${qemu_arm}:F"
     echo "Registering qemu-arm for binfmt_misc..."
-    sudo bash -c "${reg}" 2>/dev/null || true
+    sudo bash -c "echo '${reg}' > /proc/sys/fs/binfmt_misc/register" 2>/dev/null || true
   fi
 fi
 
